508f4e2f35f95cb699232bcf13120db1f07e0f9d
rosenfield <rosenfield@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2006-07-12 05:12:09 +0000

    Create Delphi 10 package for SynEdit.

--- components/synedit/Source/SynEdit.pas
+++ components/synedit/Source/SynEdit.pas
@@ -903,7 +903,11 @@ type
 
 implementation
 
-{$R SynEdit.res}
+{ Commented out and added to project as source file instead
+  to fix HeidiSQL.dpr ".res file not found" compile error with Delphi 10. }
+{$ifndef COMPILER_10_UP}
+  {$R SynEdit.res}
+{$endif}
 
 uses
 {$IFDEF SYN_CLX}
