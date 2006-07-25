671cebb73e9687fc86380e4e6b3cd747b0041a8c
ansgarbecker <ansgarbecker@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2006-07-25 21:24:44 +0000

    - Removed ThemeManager as it only makes problems in source distribution for Delphi-versions 7+

--- components/thememgr/Delphi/D6.bat
+++
@@ -1,2 +0,0 @@
-copy ThemeManagerD6_NonPersonal.dpk ThemeManagerD6.dpk  
-copy ThemeManagerD6D_NonPersonal.dpk ThemeManagerD6D.dpk  
--- components/thememgr/Delphi/D6Personal.bat
+++
@@ -1,2 +0,0 @@
-copy ThemeManagerD6_Personal.dpk ThemeManagerD6.dpk  
-copy ThemeManagerD6D_Personal.dpk ThemeManagerD6D.dpk  
