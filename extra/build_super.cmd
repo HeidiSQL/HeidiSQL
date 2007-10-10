50f566cdd1172883c97ab471a9b644d2d8c9b494
rosenfield <rosenfield@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2007-10-10 11:04:21 +0000

    Automate build, allowing it to find the relevant compiler et cetera by itself.

--- extra/build_super.cmd
+++ extra/build_super.cmd
@@ -2,129 +2,133 @@
 setlocal enableextensions
 setlocal enabledelayexpansion
 
-CLS
+cls
 
-REM =============================================
-REM
-REM Set compiler_dir and compiler_version appropriately via command line.
-REM Example:
-REM  build_super.cmd C:\path\to\delphi\bin\directory\ delphi11
-REM
+:test_dcc32
+dcc32.exe --version >NUL: 2>NUL:
+if %errorlevel% == 0 goto test_svn
 
-set compiler_dir=%1
-set package_dir=%2
+:dcc32_not_found
+echo Error: Delphi compiler 'dcc32.exe' was not found in PATH.
+echo.
+echo Please install Delphi.  When installing, Delphi should modify
+echo your system path to include the location of this file.
+echo.
+echo See also:
+echo http://www.codegear.com/downloads/free/delphi
+echo.
+pause > NUL:
+goto :eof
 
-REM =============================================
+:test_svn
+svnversion.exe --version >NUL: 2>NUL:
+if %errorlevel% == 0 goto test_libs
 
-IF %compiler_dir%. == . GOTO nostart
-IF NOT %package_dir%. == . GOTO start
+:svn_not_found
+echo Error: Subversion executable 'svnversion.exe' was not found in PATH.
+echo.
+echo Please install Subversion.  When installing, Subversion should modify
+echo your system path to include the location of this file.
+echo.
+echo See also:
+echo http://subversion.tigris.org/servlets/ProjectDocumentList?folderID=91^&filter=setup.exe
+echo.
+pause > NUL:
+goto :eof
 
-:nostart
-ECHO =================== Note ===================
-ECHO If you are being asked about paths, you can also enter them
-ECHO directly into the build script, or provide them on the command line.
-ECHO.
-ECHO For help with building individual packages, see the developer readme.
-ECHO ============================================
-ECHO.
-ECHO.
+:locate_dcc32
+if not "%compiler_dir%" == "none" goto :eof
+pushd %~f1
+if exist dcc32.exe set compiler_dir=found
+if "%compiler_dir%" == "found" cd ..
+if "%compiler_dir%" == "found" set compiler_dir=%CD%
+popd
+goto :eof
 
-IF %compiler_dir%. == . SET /P compiler_dir=Please enter path to delphi bin folder: 
-IF %package_dir%. == . SET /P package_dir=Please enter compiler version (eg delphi11): 
+:test_libs
+SET compiler_dir=none
+for %%p in ("%PATH:;=" "%") do call :locate_dcc32 %%p
+if "%compiler_dir%" == "none" (goto dcc32_not_found) else (goto test_packages)
 
-set compiler_dir=%compiler_dir:"=%
-set package_dir=%package_dir:"=%
+:test_packages
+set package_dir=none
+dcc32.exe --version | find "18.0" >NUL:
+if %errorlevel% == 0 set package_dir=delphi10
+dcc32.exe --version | find "18.5" >NUL:
+if %errorlevel% == 0 set package_dir=delphi11
+if "%package_dir%" == "none" (goto unknown_version) else (goto init)
 
-IF "%compiler_dir%" == "" GOTO usage
-IF "%package_dir%" == "" GOTO usage
-GOTO start
+:unknown_version
+echo Error: Unknown version of Delphi compiler 'dcc32.exe'!
+echo.
+echo At present, only Delphi 10 (Borland Developer Studio 2006)
+echo and Delphi 11 (CodeGear Delphi 2007).
+echo.
+echo Please install one of those...
+echo.
+echo If you think you are receiving this message in error,
+echo please inform the developers at heidisql-devel@sourceforge.net.
+echo.
+pause >NUL:
+goto :eof
 
-:usage
-ECHO.
-ECHO Please pass compiler_dir AND package_dir via
-ECHO command line or supply them when prompted.
-ECHO.
-PAUSE
-ECHO.
-GOTO end
-
-:start
+:init
 set start_time=%DATE% %TIME%
 set start_dir=%CD%
 cd ..
 set base_dir=%CD%
-cd %compiler_dir%
-set compiler_bin_dir=%CD%
-cd ..
-set compiler_dir=%CD%
-set compiler=%compiler_bin_dir%\dcc32.exe
 
+set compiler=dcc32.exe
 set params=-aWinTypes=Windows;WinProcs=Windows;DbiProcs=BDE;DbiTypes=BDE;DbiErrs=BDE
 set params=%params% -B
-set params=%params% -u"%compiler_dir%\lib;%compiler_dir%\lib\Obj;%base_dir%\components\zeosdbo\build;%base_dir%\components\virtualtreeview\build;%base_dir\%components\synedit\build;%base_dir%\components\smdbgrid\build;%base_dir%\components\heidisql\build;%base_dir%\components\edbimage\build;"
-set params=%params% -i"%base_dir%\components\compilerdetection;%base_dir%source"
+set params=%params% -u"%compiler_dir%\lib;%compiler_dir%\lib\obj;%base_dir%\components\zeosdbo\build;%base_dir%\components\virtualtreeview\build;%base_dir\%components\synedit\build;%base_dir%\components\smdbgrid\build;%base_dir%\components\heidisql\build;%base_dir%\components\edbimage\build"
+set params=%params% -i"%base_dir%\components\compilerdetection"
 set params=%params% -LE"%base_dir%\build"
 set params=%params% -LN"%base_dir%\build"
-set params=%params% -r"%base_dir%\source;%base_dir%\components\smdbgrid\Resources;%base_dir%\components\synedit\resources;%base_dir%\components\synedit\Source;%base_dir%\components\virtualtreeview\Resources;%base_dir%\components\edbimage\resources"
+set params=%params% -r"%base_dir%\components\smdbgrid\Resources;%base_dir%\components\synedit\resources;%base_dir%\components\virtualtreeview\Resources;%base_dir%\components\edbimage\resources"
 
 echo.
 echo Base directory:          %base_dir%
 echo Compiler directory:      %compiler_dir%
-echo Compiler bin directory:  %compiler_bin_dir%
-echo Compiler binary:         %compiler%
 echo.
 
-rem Switch to base directory
-cd %base_dir%
-
+:start
 rem Delete old binaries
+cd /d "%base_dir%"
 del /S *.dcu
 del /S *.dcp
 del /S *.bpl
+del /S out\heidisql.exe
+
+:find_wcver
+rem Unix tool, may not handle Windows paths well, so go to base directory and use dot.
+cd /d "%base_dir%"
+for /f "usebackq" %%s in (`svnversion.exe . ^|^| ECHO unknown`) DO SET wcver=WC %%s
+if "%wcver%" == "WC unknown" (set wcver=unknown) else (goto insert_wcver)
+
+:svnversion_failure
+rem Non-fatal, continue if this happens.
+echo Error: svnversion failed - run this step manually to see what went wrong?
 echo.
 
-SET wcver=unknown
-REM Check that svnversion exists
-set svndir=%ProgramFiles%\Subversion\bin
-"%svndir%\svnversion" --version > NUL:
-IF %errorlevel% == 0 GOTO svn_good
-set svndir=%ProgramFiles(x86)%\Subversion\bin
-"%svndir%\svnversion" --version > NUL:
-IF %errorlevel% == 0 GOTO svn_good
-
-ECHO Please install subversion from http://subversion.tigris.org/
-ECHO and make sure that svnversion.exe is in:
-ECHO "%ProgramFiles%\Subversion\bin\"
-ECHO.
-GOTO after_wcver
-
-:svn_good
-rem Unix tool, may not handle Windows paths well, so go to directory and use dot.
-CD "%base_dir%"
-FOR /F "usebackq" %%s IN (`"%svndir%\svnversion" . ^|^| ECHO unknown`) DO SET wcver=WC %%s
-IF NOT "%wcver%" == "WC unknown" GOTO go_wcver
-SET wcver=unknown
-ECHO svnversion failure - run this step manually to see what went wrong?
-ECHO.
-GOTO after_wcver
-
-:go_wcver
+:insert_wcver
 rem Put WC version or "unknown" into main.pas
 %base_dir%\extra\sed\sed.exe "s/\$Revision.*\$/\$Revision: %WCVER% \$/g" -i "%base_dir%\source\main.pas"
-IF NOT %errorlevel% == 0 GOTO sedfail
+if not %errorlevel% == 0 goto sedfail
 %base_dir%\extra\sed\sed.exe "s/\$Rev[^i].*\$/\$Rev: %WCVER% \$/g" -i "%base_dir%\source\main.pas"
-IF NOT %errorlevel% == 0 GOTO sedfail
-GOTO wc_ver_good
+if not %errorlevel% == 0 goto sedfail
+goto after_wcver
 
 :sedfail
-ECHO SED failure - run this step manually to see what went wrong?
-GOTO end
+echo Error: SED failure - run this step manually to see what went wrong?
+echo.
+goto end
 
 :after_wcver
 echo.
 
 rem Build EDBImage
-cd "%base_dir%\components\edbimage\packages\%package_dir%\"
+cd /d "%base_dir%\components\edbimage\packages\%package_dir%\"
 "%compiler%" -N0"..\..\build" %params% VCLSer.dpk
 if not %errorlevel% == 0 goto end
 "%compiler%" -N0"..\..\build" %params% DCLSer.dpk
@@ -132,13 +136,13 @@ if not %errorlevel% == 0 goto end
 
 
 rem Build SMDBGrid
-cd "%base_dir%\components\smdbgrid\packages\%package_dir%\"
+cd /d "%base_dir%\components\smdbgrid\packages\%package_dir%\"
 "%compiler%" -N0"..\..\build" %params% SMDBGridComponents.dpk
 if not %errorlevel% == 0 goto end
 
 
 rem Build SynEdit
-cd "%base_dir%\components\synedit\packages\%package_dir%\"
+cd /d "%base_dir%\components\synedit\packages\%package_dir%\"
 rem -Q = Quiet compile
 rem      This is a workaround for avoiding error D21153
 rem      see here: http://qc.borland.com/wc/qcmain.aspx?d=44731
@@ -147,7 +151,7 @@ if not %errorlevel% == 0 goto end
 
 
 rem Build ZeosDBO
-cd "%base_dir%\components\zeosdbo\packages\%package_dir%\"
+cd /d "%base_dir%\components\zeosdbo\packages\%package_dir%\"
 "%compiler%" -N0"..\..\build" -Q %params% ZCore.dpk
 if not %errorlevel% == 0 goto end
 "%compiler%" -N0"..\..\build" -Q %params% ZPlain.dpk
@@ -159,26 +163,30 @@ if not %errorlevel% == 0 goto end
 "%compiler%" -N0"..\..\build" -Q %params% ZComponent.dpk
 if not %errorlevel% == 0 goto end
 
+
 rem Build HeidiComponents
-cd "%base_dir%\components\heidisql\packages\%package_dir%\"
+cd /d "%base_dir%\components\heidisql\packages\%package_dir%\"
 "%compiler%" -N0"..\..\build" %params% HeidiComponents.dpk
 if not %errorlevel% == 0 goto end
 
 
 rem Build VirtualTreeView
-cd "%base_dir%\components\virtualtreeview\packages\%package_dir%\"
+cd /d "%base_dir%\components\virtualtreeview\packages\%package_dir%\"
 "%compiler%" -N0"..\..\build" %params% VirtualTreesR.dpk
 if not %errorlevel% == 0 goto end
 
 
 rem Build main executable
-cd "%base_dir%\packages\%package_dir%\"
+cd /d "%base_dir%\packages\%package_dir%\"
 "%compiler%" -N0"..\..\build" -e"%base_dir%\out" %params% heidisql.dpr
 if not %errorlevel% == 0 goto end
+
 
 echo.
 echo Started:   %start_time%
 echo Finished:  %DATE% %TIME%
+echo.
+
 
 :end
-cd "%base_dir%\extra" 2>NUL:
+cd /d "%start_dir%" 2>NUL:
