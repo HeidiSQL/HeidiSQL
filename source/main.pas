6c5b47a5fc61cd35972e39d0fcb19b7a7f037a89
Ansgar Becker <ansgarbecker@users.noreply.github.com>
2007-10-12 20:19:15 +0000

    Tag for 3.1 RC1

--- source/main.pas
+++ source/main.pas
@@ -215,7 +215,7 @@ var
   StatusText          : String = 'Initializing...';
   StatusIconIndex     : Integer = 43;
   loadsqlfile         : boolean = true;               // load sql-file into query-memo at startup?
-  appversion          : String = 'x.y $Rev$';
+  appversion          : String = '3.1 RC1 $Rev$';
   DirnameCommonAppData,
   DirnameUserAppData,
   DIRNAME_SNIPPETS,
