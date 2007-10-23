bf1d8a9b5c17da087bdf3c79c9f7b7adb66a2fd3
rosenfield <rosenfield@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2007-10-23 12:58:23 +0000

    Cosmetic: silence standard output from copy command - errors should still slip through.

--- trunk/extra/install_ide_components.cmd
+++ trunk/extra/install_ide_components.cmd
@@ -93,7 +93,7 @@ rem    (technically a regression since Delphi 7 worked OK.)
 rem
 rem    see here: http://qc.borland.com/wc/qcmain.aspx?d=23225
 rem
-copy /y "%base_dir%\components\%1\build\*.bpl" "%USERPROFILE%\My Documents\Borland Studio Projects\Bpl\"
+copy /y "%base_dir%\components\%1\build\*.bpl" "%USERPROFILE%\My Documents\Borland Studio Projects\Bpl\" >NUL:
 %start_dir%\IdeBplInstall\IdeBplInstall.exe "%base_dir%\components\%1\build\%2%package_suffix%.bpl" "%1"
 set err=%errorlevel%
 echo.
