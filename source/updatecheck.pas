32a4c08a8b225d174eb9e93486b2b9b128d99e3c
Ansgar Becker <ansgarbecker@users.noreply.github.com>
2008-02-14 20:24:02 +0000

    Enhancements and fixes for updatecheck dialog: - Fix URL opened by btnBuild - Fetch and display date, revision and note of release/build - Put the download buttons into separated TGroupBoxes and add a TMemo for Notes above them - Enable a download button if "download revision" > "current revision". Enables a more exact comparision, especially for releases. - Remove dodgy "refresh" icon on form.
    If you stumble across this compiler notice: "FileAge(File)" is deprecated, while the used overloaded method "FileAge(File, DateTime)" is not. See also: http://groups.google.com/group/borland.public.delphi.rtl.win32/browse_thread/thread/9792bef1ee37a298/75b96783bb4862ee

--- source/updatecheck.pas
+++ source/updatecheck.pas
@@ -4,21 +4,24 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Forms,
-  Dialogs, StdCtrls, ExtActns, IniFiles, Controls, ExtCtrls, GIFImg;
+  Dialogs, StdCtrls, ExtActns, IniFiles, Controls, Graphics;
 
 type
   TfrmUpdateCheck = class(TForm)
-    btnRelease: TButton;
-    btnBuild: TButton;
-    lblStatus: TLabel;
     btnCancel: TButton;
-    Image1: TImage;
+    groupBuild: TGroupBox;
+    btnBuild: TButton;
+    groupRelease: TGroupBox;
+    btnRelease: TButton;
+    lblStatus: TLabel;
+    memoRelease: TMemo;
+    memoBuild: TMemo;
     procedure btnBuildClick(Sender: TObject);
     procedure btnReleaseClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
   private
     { Private declarations }
-    d : TDownLoadURL;
+    CheckfileDownload : TDownLoadURL;
     ReleaseURL, BuildURL : String;
     procedure Status(txt: String);
     procedure ReadCheckFile;
@@ -56,27 +59,30 @@ begin
   Status('Initiating ... ');
   Caption := 'Check for '+APPNAME+' updates ...';
 
-  // Detect temp directory
-  GetTempPath(MAX_PATH, @TempPath);
-
-  // Initially hide download buttons
+  // Init GUI controls
   btnRelease.Enabled := False;
   btnBuild.Enabled := False;
   btnCancel.Enabled := False;
+  memoRelease.Clear;
+  memoBuild.Clear;
 
-  d := TDownLoadURL.Create(Self);
-  d.URL := APPDOMAIN + 'updatecheck.php';
-  // Todo: find a better temp directory
-  d.Filename := StrPas(TempPath) +  APPNAME + '_updatecheck.ini';
+  // Detect temp directory
+  GetTempPath(MAX_PATH, @TempPath);
+
+  // Prepare download
+  CheckfileDownload := TDownLoadURL.Create(Self);
+  CheckfileDownload.URL := APPDOMAIN + 'updatecheck.php';
+  CheckfileDownload.Filename := StrPas(TempPath) +  APPNAME + '_updatecheck.ini';
+
+  // Download the check file
   Screen.Cursor := crHourglass;
-  Status('Downloading check file '+d.URL+' ... ');
+  Status('Downloading check file ...');
   try
-    // Download the check file
-    d.ExecuteTarget(nil);
+    CheckfileDownload.ExecuteTarget(nil);
     ReadCheckFile;
-    DeleteFile(d.Filename);
+    DeleteFile(CheckfileDownload.Filename);
   finally
-    FreeAndNil(d);
+    FreeAndNil(CheckfileDownload);
     Screen.Cursor := crDefault;
     btnCancel.Enabled := True;
   end;
@@ -89,39 +95,63 @@ end;
 procedure TfrmUpdateCheck.ReadCheckFile;
 var
   Ini : TIniFile;
-  ReleaseVersion, BuildRevision, statustxt : String;
+  ReleaseVersion : String;
+  ReleaseRevision, BuildRevision : Integer;
+  Note : String;
+  Compiled : TDateTime;
 const
   INISECT_RELEASE = 'Release';
   INISECT_BUILD = 'Build';
 begin
   Status('Reading check file ...');
-  statustxt := 'No update available.';
 
   // Read [Release] section of check file
-  Ini := TIniFile.Create(d.Filename);
+  Ini := TIniFile.Create(CheckfileDownload.Filename);
   if Ini.SectionExists(INISECT_RELEASE) then begin
     ReleaseVersion := Ini.ReadString(INISECT_RELEASE, 'Version', 'unknown');
+    ReleaseRevision := Ini.ReadInteger(INISECT_RELEASE, 'Revision', 0);
     ReleaseURL := Ini.ReadString(INISECT_RELEASE, 'URL', '');
-    statustxt := 'Release available: Version ' + ReleaseVersion + ' (yours: '+AppVersion+')' + CRLF;
+    memoRelease.Lines.Add( 'Version ' + ReleaseVersion + ' (yours: '+AppVersion+')' );
+    memoRelease.Lines.Add( 'Released: ' + Ini.ReadString(INISECT_RELEASE, 'Date', '') );
+    Note := Ini.ReadString(INISECT_RELEASE, 'Note', '');
+    if Note <> '' then
+      memoRelease.Lines.Add( 'Note: ' + Note );
     btnRelease.Caption := 'Download version ' + ReleaseVersion;
     // Enable the download button if the current version is outdated
-    btnRelease.Enabled := ReleaseVersion <> AppVersion;
+    groupRelease.Enabled := ReleaseRevision > StrToIntDef(AppRevision, 0);
+    btnRelease.Enabled := groupRelease.Enabled;
+    memoRelease.Enabled := groupBuild.Enabled;
+    if not memoRelease.Enabled then
+      memoRelease.Font.Color := cl3DDkShadow
+    else
+      memoRelease.Font.Color := clWindowText;
   end;
 
   // Read [Build] section of check file
   if Ini.SectionExists(INISECT_BUILD) then begin
-    BuildRevision := Ini.ReadString(INISECT_BUILD, 'Revision', 'unknown');
+    BuildRevision := Ini.ReadInteger(INISECT_BUILD, 'Revision', 0);
     BuildURL := Ini.ReadString(INISECT_BUILD, 'URL', '');
-    statustxt := statustxt + 'Build available: Revision ' + BuildRevision + ' (yours: '+AppRevision+')' + CRLF;
-    btnBuild.Caption := 'Download build ' + BuildRevision;
+    memoBuild.Lines.Add( 'Revision ' + IntToStr(BuildRevision) + ' (yours: '+AppRevision+')' );
+    FileAge(ParamStr(0), Compiled);
+    memoBuild.Lines.Add( 'Compiled: ' + Ini.ReadString(INISECT_BUILD, 'Date', '') + ' (yours: '+DateToStr(Compiled)+')' );
+    Note := Ini.ReadString(INISECT_BUILD, 'Note', '');
+    if Note <> '' then
+      memoBuild.Lines.Add( 'Note: ' + Note );
+    btnBuild.Caption := 'Download build ' + IntToStr(BuildRevision);
     // A new release should have priority over a new nightly build.
     // So the user should not be able to download a newer build here
     // before having installed the new release.
-    btnBuild.Enabled := (BuildRevision <> AppRevision) and (not btnRelease.Enabled);
+    groupBuild.Enabled := (BuildRevision > StrToIntDef(AppRevision, 0)) and (not btnRelease.Enabled);
+    btnBuild.Enabled := groupBuild.Enabled;
+    memoBuild.Enabled := groupBuild.Enabled;
+    if not memoBuild.Enabled then
+      memoBuild.Font.Color := cl3DDkShadow
+    else
+      memoBuild.Font.Color := clWindowText;
   end;
 
-  // Display summary
-  Status(statustxt);
+  // Clear status label
+  Status('');
 end;
 
 
@@ -144,7 +174,7 @@ end;
 }
 procedure TfrmUpdateCheck.btnBuildClick(Sender: TObject);
 begin
-  ShellExec(ReleaseURL);
+  ShellExec(BuildURL);
 end;
 
 end.
