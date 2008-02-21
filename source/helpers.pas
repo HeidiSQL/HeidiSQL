77880142582b1e0cae118dc7a69b2e01aea7e495
Francisco Ernesto Teixeira <fco.ernesto@gmail.com>
2008-02-21 03:01:10 +0000

    Added methods that enable the update of running main application just giving the direction of newest main application file.

--- source/helpers.pas
+++ source/helpers.pas
@@ -85,12 +85,16 @@ type
   function TColorToHex( Color : TColor ): string;
   function GetVTCaptions( VT: TVirtualStringTree; OnlySelected: Boolean = False; Column: Integer = 0 ): TStringList;
   function Pos2(const Needle, HayStack: string; const StartPos: Integer) : Integer;
+  procedure ExtractUpdater;
+  procedure UpdateItWith(const _file: String);
 
 var
   MYSQL_KEYWORDS             : TStringList;
 
 
 implementation
+
+{$R updater.res}
 
 uses main;
 
@@ -2132,6 +2136,85 @@ begin
   Result := Pos(Needle, NewHayStack);
   if Result > 0 then Result := Result + StartPos - 1;
 end;
+
+
+
+{**
+  Extract the updater from resource.
+}
+procedure ExtractUpdater;
+var
+  ri: HRSRC;
+  rh: THandle;
+  bf: PChar;
+  ms: TMemoryStream;
+begin
+  // look for resource UPDATER
+  ri := FindResource( HInstance, 'UPDATER', 'EXE' );
+
+  // define the Handle
+  rh := LoadResource( HInstance, ri );
+  
+  if ( rh <> 0 ) then
+  begin
+    // alloc memory space
+    ms := TMemoryStream.Create();
+    try
+      ms.Clear();
+      bf := LockResource( rh );
+
+      // load the resource on memory stream
+      ms.WriteBuffer( bf[0], SizeOfResource( HInstance, ri ) );
+      ms.Seek( 0, 0 );
+
+      // save the resource like executable
+      ms.SaveToFile( ExtractFilePath( Application.ExeName ) + 'updater.exe' );
+    finally
+      // free memory
+      UnlockResource( rh );
+      FreeResource( rh );
+      ms.Free();
+    end;
+  end;
+end;
+
+
+
+{**
+  Update the current application with the file.
+
+  @param _file the file that will replace the current application
+}
+procedure UpdateItWith(const _file: String);
+var
+  app: String;
+begin
+  // sanitize: try to remove the oldest updater
+  DeleteFile( PAnsiChar( ExtractFilePath( Application.ExeName ) + 'updater.exe' ) );
+
+  // retrive the application name without extension
+  app := Copy( Application.Exename, 1, ( Length( Application.Exename ) - 4 ) );
+
+  // sanitize: try to remove the oldest file
+  DeleteFile( PAnsiChar( app + '.old' ) );
+
+  // verify the file existence
+  if ( FileExists( _file ) ) then
+  begin
+    // copy to current application folder the newest file
+    CopyFile( PChar( _file ), PChar( app + '.new' ), False );
+
+    // extract the updater from current application resource
+    ExtractUpdater();
+
+    // call the updater
+    WinExec( PAnsiChar( '"' + ExtractFilePath( Application.ExeName ) + 'updater.exe" "' + app + '"' ), 0 );
+
+    // stop running the current application
+    Application.Terminate();
+  end;
+end;
+
 
 
 initialization
