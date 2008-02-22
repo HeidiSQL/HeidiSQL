d1648c2bb25afeeb0b7eaedc5f99451e61e9742c
servertude <servertude@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2008-02-22 04:18:21 +0000

    initial upload

---
+++ extra/jheidi/ajl.cmd
@@ -0,0 +1,2 @@
+java -cp .;classes;lib/ajl.jar;%JAVA_HOME%/lib/tools.jar;lib/editor.jar;lib/mysql-connector-java-5.0.7-bin.jar;lib/log4j-1.2.13.jar -Dlog4jconfig=logs/log4jconfig.xml com.rendion.ajl.AjlScript %1.ajl %2 %3 % 4 %5 %6 %7 %8 %9
+
