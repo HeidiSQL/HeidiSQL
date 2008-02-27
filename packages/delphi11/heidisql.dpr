2f30ee2e68845ca79d9211b6433caf7cca59de4c
Ansgar Becker <ansgarbecker@users.noreply.github.com>
2008-02-27 19:31:58 +0000

    Simplify updatecheck: Make unit updatedownload superflous by moving relevant code to updatecheck.pas. Resign from displaying a progressbar for the mostly short download time, instead use the existing status label for a textual progress information.

--- packages/delphi11/heidisql.dpr
+++ packages/delphi11/heidisql.dpr
@@ -35,8 +35,7 @@ uses
   data_sorting in '..\..\source\data_sorting.pas' {DataSortingForm},
   runsqlfile in '..\..\source\runsqlfile.pas' {RunSQLFileForm},
   createdatabase in '..\..\source\createdatabase.pas' {CreateDatabaseForm},
-  updatecheck in '..\..\source\updatecheck.pas' {frmUpdateCheck},
-  updatedownload in '..\..\source\updatedownload.pas' {frmUpdateDownload};
+  updatecheck in '..\..\source\updatecheck.pas' {frmUpdateCheck};
 
 {$R *.RES}
 
