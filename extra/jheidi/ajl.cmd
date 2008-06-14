f77ed13a4d49b10cde25086d201eef946079edba
servertood <servertood@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2008-06-14 22:31:47 +0000

    fix windows cmd args

--- extra/jheidi/ajl.cmd
+++ extra/jheidi/ajl.cmd
@@ -1,2 +1,2 @@
-java -DstartEDT=true -cp .;classes;lib/ajl.jar;%JAVA_HOME%/lib/tools.jar;lib/editor.jar;lib/swing-worker-1.1.jar;lib/mysql-connector-java-5.0.7-bin.jar;lib/log4j-1.2.13.jar -Dlog4jconfig=logs/log4jconfig.xml com.rendion.ajl.AjlScript %1.ajl %2 %3 % 4 %5 %6 %7 %8 %9
+java -DstartEDT=true -cp .;classes;lib/ajl.jar;lib/tools.jar;lib/editor.jar;lib/swing-worker-1.1.jar;lib/mysql-connector-java-5.0.7-bin.jar;lib/log4j-1.2.13.jar -Dlog4jconfig=logs/log4jconfig.xml com.rendion.ajl.AjlScript %1 %2 %3 %4 %5 %6 %7 %8 %9
 
