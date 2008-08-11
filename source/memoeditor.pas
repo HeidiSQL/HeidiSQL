fa17d1eb8de14be56d3ab57c35b6736684ed6c4d
Ansgar Becker <ansgarbecker@users.noreply.github.com>
2008-08-11 23:16:46 +0000

    Add a toolbar with some useful buttons to the memo editor: Wrap long lines, Load textfile, Cancel, Apply Plus: a label showing the current and the allowed text length

--- source/memoeditor.pas
+++ source/memoeditor.pas
@@ -3,15 +3,27 @@ unit memoeditor;
 interface
 
 uses
-  Windows, Classes, Graphics, Controls, Forms, StdCtrls, TntStdCtrls, Registry, VirtualTrees;
+  Windows, Classes, Graphics, Controls, Forms, StdCtrls, TntStdCtrls, Registry, VirtualTrees,
+  ComCtrls, ToolWin, Dialogs;
 
 {$I const.inc}
 
 type
   TfrmMemoEditor = class(TForm)
     memoText: TTntMemo;
+    tlbStandard: TToolBar;
+    btnWrap: TToolButton;
+    btnLoadText: TToolButton;
+    btnApply: TToolButton;
+    btnCancel: TToolButton;
+    lblTextLength: TLabel;
+    procedure btnApplyClick(Sender: TObject);
+    procedure btnCancelClick(Sender: TObject);
+    procedure btnLoadTextClick(Sender: TObject);
+    procedure btnWrapClick(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure memoTextChange(Sender: TObject);
     procedure memoTextKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
   private
     { Private declarations }
@@ -52,9 +64,12 @@ begin
   Top := TopLeft.Y;
   // Hide window caption
   SetWindowLong(Handle, GWL_STYLE, GetWindowLong( Handle, GWL_STYLE ) and not WS_CAPTION );
+  // Fix label position:
+  lblTextLength.Top := tlbStandard.Top + (tlbStandard.Height-lblTextLength.Height) div 2;
   SetWindowSizeGrip(Handle, True);
   memoText.SelectAll;
   memoText.SetFocus;
+  memoTextChange(Sender);
 end;
 
 
@@ -70,6 +85,56 @@ begin
     // Apply changes and end editing by Ctrl + Enter
     VK_RETURN: if ssCtrl in Shift then Tree.EndEditNode;
   end;
+end;
+
+procedure TfrmMemoEditor.btnWrapClick(Sender: TObject);
+begin
+  Screen.Cursor := crHourglass;
+  if memoText.ScrollBars = ssBoth then
+    memoText.ScrollBars := ssVertical
+  else
+    memoText.ScrollBars := ssBoth;
+  TToolbutton(Sender).Down := memoText.ScrollBars = ssVertical;
+  Screen.Cursor := crDefault;
+end;
+
+
+procedure TfrmMemoEditor.btnLoadTextClick(Sender: TObject);
+var
+  d: TOpenDialog;
+begin
+  d := TOpenDialog.Create(Self);
+  d.Filter := 'Textfiles (*.txt)|*.txt|All files (*.*)|*.*';
+  d.FilterIndex := 0;
+  if d.Execute then try
+    Screen.Cursor := crHourglass;
+    memoText.Text := ReadTextFile(d.FileName);
+    if (memoText.MaxLength > 0) and (Length(memoText.Text) > memoText.MaxLength) then
+      memoText.Text := copy(memoText.Text, 0, memoText.MaxLength);
+  finally
+    Screen.Cursor := crDefault;
+  end;
+  d.Free;
+end;
+
+
+procedure TfrmMemoEditor.btnCancelClick(Sender: TObject);
+begin
+  TCustomVirtualStringTree(Parent).CancelEditNode;
+end;
+
+
+procedure TfrmMemoEditor.btnApplyClick(Sender: TObject);
+begin
+  TCustomVirtualStringTree(Parent).EndEditNode;
+end;
+
+
+procedure TfrmMemoEditor.memoTextChange(Sender: TObject);
+begin
+  lblTextLength.Caption := FormatNumber(Length(memoText.Text)) + ' characters.';
+  if memoText.MaxLength > 0 then
+    lblTextLength.Caption := lblTextLength.Caption + ' (Max: '+FormatNumber(memoText.MaxLength)+')';
 end;
 
 end.
