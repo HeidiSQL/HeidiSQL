e10bae1c472efc6e1708ec17d68515573ab9970a
servertood <servertood@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2008-11-07 21:39:49 +0000

    use a real message loop instead of just handling WM_PAINT messages this finally gets rid of hourglass

--- extra/jheidi/etc/mutex/mutex.cpp
+++ extra/jheidi/etc/mutex/mutex.cpp
@@ -2,24 +2,10 @@
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nShowCmd)
 {
-	
-
-	//looks redundant but we actually have to get the message loop running asap
-	//to avoid a long hourglass being shown.
-	MSG msg;
-	
-	while (PeekMessage(&msg, NULL, WM_PAINT, WM_PAINT, PM_REMOVE))
-			DispatchMessage(&msg);
-
-	HANDLE h = GetCurrentProcess();
-    
-    while (WaitForSingleObject(h,100) == WAIT_TIMEOUT)
-    {
-		while (PeekMessage(&msg, NULL, WM_PAINT, WM_PAINT, PM_REMOVE))
-			DispatchMessage(&msg);
-	}
-    CloseHandle( h );
-
-		
-
+   MSG Msg;
+   while(GetMessage(&Msg, NULL, 0, 0) > 0)
+   {
+    TranslateMessage(&Msg);
+    DispatchMessage(&Msg);
+   }
 }
