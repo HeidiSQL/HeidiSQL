ed31558e6580ca1512e5cd3059446b4244e36139
servertood <servertood@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2008-12-21 00:56:17 +0000

    add oracle jar to dev classpath

--- trunk/extra/jheidi/ajl.cmd
+++ trunk/extra/jheidi/ajl.cmd
@@ -1,3 +1,3 @@
 if not exist classes mkdir classes
-java -DstartEDT=true -cp .;classes;lib/ajl.jar;lib/tools.jar;lib/editor.jar;lib/swing-worker-1.1.jar;lib/mysql-connector-java-5.0.7-bin.jar;lib/log4j-1.2.13.jar -Dlog4jconfig=logs/log4jconfig.xml com.rendion.ajl.AjlScript %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -DstartEDT=true -DusePrecompiledClassFiles=false -cp .;classes;lib/ajl.jar;lib/tools.jar;lib/editor.jar;lib/swing-worker-1.1.jar;lib/mysql-connector-java-5.0.7-bin.jar;lib/ojdbc14.jar;lib/log4j-1.2.13.jar -Dlog4jconfig=logs/log4jconfig.xml com.rendion.ajl.AjlScript %1 %2 %3 %4 %5 %6 %7 %8 %9
 
