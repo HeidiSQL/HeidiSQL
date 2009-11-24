e7a099401c1d6a5279baffb2839a63a3deca3483
ansgar.becker <ansgar.becker@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2009-11-24 18:36:57 +0000

    Do not add thousand separators to zerofilled numbers

--- trunk/source/helpers.pas
+++ trunk/source/helpers.pas
@@ -1845,11 +1845,15 @@ var
 begin
   Result := str;
   Result := StringReplace(Result, '.', DecimalSeparator, [rfReplaceAll]);
+  // Do not add thousand separators to zerofilled numbers
+  if ((Length(Result) >= 1) and (Result[1] = '0'))
+    or ((Length(Result) >= 1) and (Result[1] = '-') and (Result[1] = '0')) then
+    Exit;
   p := Pos(DecimalSeparator, Result);
   if p = 0 then p := Length(Result)+1;
   Left := 2;
   if (Length(Result) >= 1) and (Result[1] = '-') then
-    Left := 3; 
+    Left := 3;
   if p > 0 then for i:=p-1 downto Left do begin
     if (p-i) mod 3 = 0 then
       Insert(ThousandSeparator, Result, i);
