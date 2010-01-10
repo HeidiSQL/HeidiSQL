1a4db545c5d23873441a5484707b20ecc7bbcf33
Ansgar Becker <ansgarbecker@users.noreply.github.com>
2010-01-10 16:22:53 +0000

    Refactor layout and code of about dialog.

--- source/about.pas
+++ source/about.pas
@@ -1,69 +1,42 @@
 unit About;
 
-
 // -------------------------------------
 // About-box
 // -------------------------------------
 
-
 interface
 
 uses
-  Windows, Classes, Graphics, Forms, Controls, StdCtrls, ExtCtrls, SysUtils, ComCtrls, GIFImg;
-
+  Windows, Classes, Graphics, Forms, Controls, StdCtrls, ExtCtrls, SysUtils, ComCtrls, pngimage;
 
 type
   TAboutBox = class(TForm)
-    PanelLogos: TPanel;
-    ImageMysql: TImage;
-    ButtonClose: TButton;
-    PanelMain: TPanel;
-    ProductName: TLabel;
-    LabelVersion: TLabel;
-    LabelCompiled: TLabel;
-    LabelWebpage: TLabel;
-    PageControlTexts: TPageControl;
-    tabAuthors: TTabSheet;
-    ButtonBoard: TButton;
-    ImageHeidisql: TImage;
-    MemoThanks: TMemo;
-    tabThanks: TTabSheet;
+    btnClose: TButton;
+    btnForum: TButton;
     MemoAuthors: TMemo;
-    ImageDonate: TImage;
+    lblAppName: TLabel;
+    lblAppVersion: TLabel;
+    lblAppCompiled: TLabel;
+    lblAppWebpage: TLabel;
     btnUpdateCheck: TButton;
+    ImageHeidisql: TImage;
+    btnDonate: TButton;
+    lblAppDescription: TLabel;
     procedure OpenURL(Sender: TObject);
     procedure MouseOver(Sender: TObject; Shift: TShiftState; X, Y: Integer);
     procedure FormShow(Sender: TObject);
-
   private
     { Private declarations }
-
   public
     { Public declarations }
   end;
 
-
-  procedure AboutWindow( AOwner: TComponent );
-
-// -----------------------------------------------------------------------------
 implementation
-
 
 uses
   main, helpers;
 
-
 {$R *.DFM}
-
-
-procedure AboutWindow( AOwner: TComponent );
-var
-  f    : TAboutBox;
-begin
-  f := TAboutBox.Create( AOwner );
-  f.ShowModal(); // don't care about result
-  FreeAndNil(f);
-end;
 
 
 procedure TAboutBox.OpenURL(Sender: TObject);
@@ -75,7 +48,7 @@ end;
 procedure TAboutBox.MouseOver(Sender: TObject; Shift: TShiftState;
   X, Y: Integer);
 begin
-  LabelWebpage.Font.Color := clBlue;
+  lblAppWebpage.Font.Color := clBlue;
   if (Sender is TLabel) then
   begin
     TLabel(Sender).Font.Color := clRed;
@@ -85,28 +58,28 @@ end;
 
 procedure TAboutBox.FormShow(Sender: TObject);
 var
-//  CompiledInt    : Integer;
   Compiled       : TDateTime;
 begin
-  // Define the current cursor like a clock
   Screen.Cursor := crHourGlass;
 
   InheritFont(Font);
-  InheritFont(ProductName.Font);
-  ProductName.Font.Size := 14;
-  InheritFont(LabelWebpage.Font);
+  InheritFont(lblAppName.Font);
+  lblAppName.Font.Size := 14;
+  InheritFont(lblAppWebpage.Font);
 
+  // Assign text
+  Caption := 'About '+AppName;
+  lblAppName.Caption := AppName;
+  lblAppDescription.Caption := AppDescription;
+  lblAppVersion.Caption := 'Version '+AppVersion;
+  FileAge(ParamStr(0), Compiled);
+  lblAppCompiled.Caption := 'Compiled on: ' + DateTimeToStr(Compiled);
+  lblAppWebpage.Caption := AppDomain;
+  lblAppWebpage.Hint := AppDomain;
   // Avoid scroll by removing blank line outside visible area in Authors text box
   MemoAuthors.Text := TrimRight(MemoAuthors.Text);
+  MemoAuthors.WordWrap := True;
 
-  // App-Version
-  LabelVersion.Caption := 'Version '+AppVersion;
-
-  // Compile-date
-  FileAge(ParamStr(0), Compiled);
-  LabelCompiled.Caption := 'Compiled on: ' + DateTimeToStr( Compiled );
-
-  // Define the current cursor like a pointer (default)
   Screen.Cursor := crDefault;
 end;
 
