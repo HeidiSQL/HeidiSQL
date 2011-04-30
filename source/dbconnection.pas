HeidiSQL/HeidiSQL

commit d1552e425885268b55e3825a5bb0a9769179ac6b

Drop default value *after* data type was altered, to avoid "can't have a default value". Fixes issue #2361.
git-svn-id: http://heidisql.googlecode.com/svn/trunk@3795 560a3889-bb33-0410-9f33-43a8a102dc09

source/table_editor.pas
@@ -484,18 +484,6 @@ begin
   end;
   AddQuery;
 
-  // Special case for removed default values on columns, which can neither be done in
-  // ALTER TABLE ... CHANGE COLUMN query, as there is no "no default" clause, nor by
-  // appending an ALTER COLUMN ... DROP DEFAULT, without getting an "unknown column" error
-  for i:=0 to FColumns.Count-1 do begin
-    if (FColumns[i].FStatus = esModified)
-      and (FColumns[i].DefaultType = cdtNothing)
-      and (FColumns[i].DataType.HasDefault)
-      then
-      Specs.Add('ALTER '+DBObject.Connection.QuoteIdent(FColumns[i].OldName)+' DROP DEFAULT');
-  end;
-  AddQuery;
-
   if editName.Text <> DBObject.Name then
     Specs.Add('RENAME TO ' + DBObject.Connection.QuoteIdent(editName.Text));
   if memoComment.Tag = ModifiedFlag then
@@ -618,6 +606,20 @@ begin
   end;
 
   AddQuery;
+
+  // Special case for removed default values on columns, which can neither be done in
+  // ALTER TABLE ... CHANGE COLUMN query, as there is no "no default" clause, nor by
+  // appending an ALTER COLUMN ... DROP DEFAULT, without getting an "unknown column" error.
+  // Also, do this after the data type was altered, if from TEXT > VARCHAR e.g.
+  for i:=0 to FColumns.Count-1 do begin
+    if (FColumns[i].FStatus = esModified)
+      and (FColumns[i].DefaultType = cdtNothing)
+      and (FColumns[i].DataType.HasDefault)
+      then
+      Specs.Add('ALTER '+DBObject.Connection.QuoteIdent(FColumns[i].OldName)+' DROP DEFAULT');
+  end;
+  AddQuery;
+
   FreeAndNil(Specs);
   Mainform.ShowStatusMsg;
   Mainform.ProgressBarStatus.Hide;

commit 52c57c84e34c5fa5ef2cfcd8da1c92ed337aa834

Fix follow up problems introduced in r3795: DROP DEFAULT on renamed column took the old column name, and the old table name is used also when it was renamed. Revert r3795, move DROP DEFAULT clauses before the main ALTER query, and introduce a .OldDataType property on TTableColumn to check if that can have a default value.

source/dbconnection.pas
@@ -230,7 +230,7 @@ type
       procedure SetStatus(Value: TEditingStatus);
     public
       Name, OldName: String;
-      DataType: TDBDatatype;
+      DataType, OldDataType: TDBDatatype;
       LengthSet: String;
       Unsigned, AllowNull, ZeroFill, LengthCustomized: Boolean;
       DefaultType: TColumnDefaultType;
@@ -2698,6 +2698,7 @@ begin
 
     // Datatype
     Col.DataType := GetDatatypeByName(UpperCase(rx.Match[2]));
+    Col.OldDataType := GetDatatypeByName(UpperCase(rx.Match[2]));
 
     // Length / Set
     // Various datatypes, e.g. BLOBs, don't have any length property

source/table_editor.pas
@@ -484,6 +484,19 @@ begin
   end;
   AddQuery;
 
+  // Special case for removed default values on columns, which can neither be done in
+  // ALTER TABLE ... CHANGE COLUMN query, as there is no "no default" clause, nor by
+  // appending an ALTER COLUMN ... DROP DEFAULT, without getting an "unknown column" error.
+  // Also, do this after the data type was altered, if from TEXT > VARCHAR e.g.
+  for i:=0 to FColumns.Count-1 do begin
+    if (FColumns[i].FStatus = esModified)
+      and (FColumns[i].DefaultType = cdtNothing)
+      and (FColumns[i].OldDataType.HasDefault)
+      then
+      Specs.Add('ALTER '+DBObject.Connection.QuoteIdent(FColumns[i].OldName)+' DROP DEFAULT');
+  end;
+  AddQuery;
+
   if editName.Text <> DBObject.Name then
     Specs.Add('RENAME TO ' + DBObject.Connection.QuoteIdent(editName.Text));
   if memoComment.Tag = ModifiedFlag then
@@ -605,19 +618,6 @@ begin
       Specs.Add('ADD '+FForeignKeys[i].SQLCode(True));
   end;
 
-  AddQuery;
-
-  // Special case for removed default values on columns, which can neither be done in
-  // ALTER TABLE ... CHANGE COLUMN query, as there is no "no default" clause, nor by
-  // appending an ALTER COLUMN ... DROP DEFAULT, without getting an "unknown column" error.
-  // Also, do this after the data type was altered, if from TEXT > VARCHAR e.g.
-  for i:=0 to FColumns.Count-1 do begin
-    if (FColumns[i].FStatus = esModified)
-      and (FColumns[i].DefaultType = cdtNothing)
-      and (FColumns[i].DataType.HasDefault)
-      then
-      Specs.Add('ALTER '+DBObject.Connection.QuoteIdent(FColumns[i].OldName)+' DROP DEFAULT');
-  end;
   AddQuery;
 
   FreeAndNil(Specs);