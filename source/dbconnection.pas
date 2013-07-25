e509ea5437b22d1abd4d1ae4a01ee1be1547804b
Ansgar Becker <ansgarbecker@users.noreply.github.com>
2013-07-25 03:59:42 +0000

    Include application name in login prompt. Fixes issue #3180.

--- source/dbconnection.pas
+++ source/dbconnection.pas
@@ -1335,7 +1335,7 @@ begin
   // Prompt for password on initial connect
   if FParameters.LoginPrompt and (not FLoginPromptDone) then begin
     Dialog := TfrmLogin.Create(Self);
-    Dialog.Caption := FParameters.SessionName;
+    Dialog.Caption := APPNAME + ' - ' + FParameters.SessionName;
     Dialog.lblPrompt.Caption := f_('Login to %s:', [FParameters.Hostname]);
     Dialog.editUsername.Text := FParameters.Username;
     Dialog.editPassword.Text := FParameters.Password;
