273cbefcb02a56e876d49cf0fd071c4944478aea
ansgar.becker <ansgar.becker@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2013-10-20 08:29:28 +0000

    Get code running in both XE + XE5 compilers: * Conditionally including units does not really work, because of a IDE bug: http://stackoverflow.com/questions/13776195/how-to-conditionally-include-some-units . So, we define a unit alias for System.Actions in XE, which we do not need in XE5. * Dynamically use the fitting numeric type per compiler for a TUpDown.OnChangingEx event.

--- trunk/source/main.pas
+++ trunk/source/main.pas
@@ -19,7 +19,7 @@ uses
   routine_editor, trigger_editor, event_editor, options, EditVar, helpers, createdatabase, table_editor,
   TableTools, View, Usermanager, SelectDBObject, connections, sqlhelp, dbconnection,
   insertfiles, searchreplace, loaddata, copytable, VTHeaderPopup, Cromis.DirectoryWatch, SyncDB, gnugettext,
-  JumpList;
+  JumpList, System.Actions;
 
 
 type
