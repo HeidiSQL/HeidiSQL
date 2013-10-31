0f2590c917026dae9032fdd2c4199f1e1f770b5b
Ansgar Becker <ansgarbecker@users.noreply.github.com>
2013-10-31 20:19:22 +0000

    Prefer Delphi's new TZipFile class over mORMot's TZipWrite for creating zipped exports (issue #3354)

--- components/mORMot/SQLite3/c.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-del sqlite3fts3.obj
-del sqlite3.obj
-\dev\bcc\bin\bcc32 -6 -O2 -c -d -DSQLITE_ENABLE_FTS3 -u- sqlite3.c
-copy sqlite3.obj sqlite3fts3.obj
-\dev\bcc\bin\bcc32 -6 -O2 -c -d -u- sqlite3.c
-rem \dev\bcc\bin\bcc32 -6 -O2 -c -d -u- -S sqlite3.c
- pause
