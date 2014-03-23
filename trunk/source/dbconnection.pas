2ca2848036e416e21f517010937b41302e4f38d2
ansgar.becker <ansgar.becker@40cadd2d-89a2-4482-8f20-0232d845d7e3>
2014-03-23 15:37:04 +0000

    Get path to cmd.exe via COMSPEC environment variable. See http://www.heidisql.com/forum.php?t=15206

--- trunk/source/dbconnection.pas
+++ trunk/source/dbconnection.pas
@@ -1129,7 +1129,7 @@ begin
         FillChar(FPlinkProcInfo, SizeOf(TProcessInformation), 0);
         FillChar(StartupInfo, SizeOf(TStartupInfo), 0);
         StartupInfo.cb := SizeOf(TStartupInfo);
-        if CreateProcess(PChar('C:\Windows\System32\cmd.exe'), PChar(PlinkCmd), nil, nil, false,
+        if CreateProcess(PChar(GetEnvironmentVariable('COMSPEC')), PChar(PlinkCmd), nil, nil, false,
           CREATE_DEFAULT_ERROR_MODE + NORMAL_PRIORITY_CLASS + CREATE_NO_WINDOW,
           nil, nil, StartupInfo, FPlinkProcInfo) then begin
           WaitForSingleObject(FPlinkProcInfo.hProcess, FParameters.SSHTimeout*1000);
