c5f655753edf449523b40801d52da271c212c867
Ansgar Becker <anse@heidisql.com>
2017-12-22 07:57:23 +0100

    Don't copy values of virtual columns when duplicating a row. Closes #53

--- source/main.pas
+++ source/main.pas
@@ -4533,6 +4533,7 @@ var
   i: Integer;
   Value: String;
   IsNull, AllowNewNode: Boolean;
+  TableCol: TTableColumn;
 begin
   Grid := ActiveGrid;
   Results := GridResult(Grid);
@@ -4558,6 +4559,9 @@ begin
           continue; // Ignore invisible key column
         if Results.ColIsPrimaryKeyPart(i) then
           continue; // Empty value for primary key column
+        TableCol := Results.ColAttributes(i);
+        if (TableCol <> nil) and (not TableCol.Virtuality.IsEmpty) then
+          continue; // Don't copy virtual column value
         Results.RecNo := DupeNum^;
         Value := Results.Col(i);
         IsNull := Results.IsNull(i);
