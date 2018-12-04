151862bc52d2443d656e89106eb6c4fd4d93661d
Ansgar Becker <anse@heidisql.com>
2018-12-04 19:39:19 +0100

    #395: fix crash in some situations - not sure how to reproduce, but reported on https://www.heidisql.com/forum.php?t=26148

--- source/main.pas
+++ source/main.pas
@@ -9057,7 +9057,7 @@ begin
   end else if (TypeCat = dtcText) or ((TypeCat in [dtcBinary, dtcSpatial]) and actBlobAsText.Checked) then begin
     InplaceEditor := TInplaceEditorLink.Create(VT);
     InplaceEditor.MaxLength := Results.MaxLength(Column);
-    InplaceEditor.TitleText := DataGrid.Header.Columns[Column].Text;
+    InplaceEditor.TitleText := Results.ColumnNames[Column];
     InplaceEditor.ButtonVisible := True;
     EditLink := InplaceEditor;
   end else if (TypeCat in [dtcBinary, dtcSpatial]) and AppSettings.ReadBool(asFieldEditorBinary) then begin
