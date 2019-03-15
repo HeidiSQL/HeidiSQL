dace95b10d16e3bb7ccb4ad610b456efec2139c1
Ansgar Becker <anse@heidisql.com>
2019-03-15 07:44:32 +0100

    Issue #433: Additional line break after SQL export comments for skipping data

--- source/tabletools.pas
+++ source/tabletools.pas
@@ -1641,10 +1641,10 @@ begin
     // Table data
     if comboExportData.Text = DATA_NO then begin
       if menuExportAddComments.Checked then
-        Output('-- '+_('Data exporting was unselected.')+CRLF, False, True, True, False, False);
+        Output('-- '+_('Data exporting was unselected.')+CRLF+CRLF, False, True, True, False, False);
     end else if MatchText(DBObj.Engine, ['MRG_MYISAM', 'FEDERATED']) then begin
       if menuExportAddComments.Checked then
-        Output('-- '+f_('Table data not exported because this is %s table which holds its data in separate tables.', [DBObj.Engine])+CRLF, False, True, True, False, False);
+        Output('-- '+f_('Table data not exported because this is %s table which holds its data in separate tables.', [DBObj.Engine])+CRLF+CRLF, False, True, True, False, False);
     end else begin
       tmp := FormatNumber(DBObj.Rows)+' rows';
       if LowerCase(DBObj.Engine) = 'innodb' then
