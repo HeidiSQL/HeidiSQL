32f399a5e037ece85dc4568da5c0ed4f92f46294
Ansgar Becker <anse@heidisql.com>
2020-06-06 13:13:46 +0200

    Fix non-ghosted icons in database tree, temporarily in VirtualTrees.Utils.pas. The right place for that fix would be in TVirtualImageList.DoDraw. Closes #1045

--- components/virtualtreeview/Source/VirtualTrees.Utils.pas
+++ components/virtualtreeview/Source/VirtualTrees.Utils.pas
@@ -1282,7 +1282,10 @@ procedure DrawImage(ImageList: TCustomImageList; Index: Integer; Canvas: TCanvas
 
 begin
   if Enabled then
-    TCustomImageListCast(ImageList).DoDraw(Index, Canvas, X, Y, Style, Enabled)
+    // HeidiSQL fix for #1045, required until Embarcadero fixes TVirtualImageList.DoDraw:
+    //TCustomImageListCast(ImageList).DoDraw(Index, Canvas, X, Y, Style, Enabled)
+    ImageList_DrawEx(ImageList.Handle, Index, Canvas.Handle, X, Y, 0, 0,
+      GetRGBColor(ImageList.BkColor), GetRGBColor(ImageList.BlendColor), Style)
   else
     DrawDisabledImage(ImageList, Canvas, X, Y, Index);
 end;
