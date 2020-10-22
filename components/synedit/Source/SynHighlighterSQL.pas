HeidiSQL/HeidiSQL

commit 7e4094d37b01c3356c6b8e65fed14fb97ab78b4b

Update SynEdit's list of MySQL function names, taken from MariaDB 10.5.5 help tables

components/synedit/Source/SynHighlighterSQL.pas
@@ -1095,76 +1095,74 @@ const
     'utf8_vietnamese_ci';
 
   // functions
+  // Taken from MariaDB 10.5.5 help tables
   MySQLFunctions: UnicodeString =
-    'ABS,ACOS,ADDDATE,ADDTIME,AES_DECRYPT,AES_ENCRYPT,ANY_VALUE,AREA,' +
-    'ASBINARY,ASCII,ASIN,ASTEXT,ASWKBASWKT,ASYMMETRIC_DECRYPT,' +
-    'ASYMMETRIC_DERIVE,ASYMMETRIC_ENCRYPT,ASYMMETRIC_SIGN,ASYMMETRIC_VERIFY,' +
-    'ATAN,ATAN2,AVG,BIN,BIT_AND,BIT_COUNT,BIT_LENGTH,BIT_OR,BIT_XOR,BUFFER,' +
-    'CAST,CEIL,CEILING,CENTROID,CHAR_LENGTH,CHARACTER_LENGTH,COALESCE,' +
-    'COERCIBILITY,COMPRESS,CONCAT,CONCAT_WS,CONNECTION_ID,CONV,CONVERT_TZ,' +
-    'CONVEXHULL,COS,COT,COUNT,CRC32,CREATE_ASYMMETRIC_PRIV_KEY,' +
-    'CREATE_ASYMMETRIC_PUB_KEY,CREATE_DH_PARAMETERS,CREATE_DIGEST,CROSSES,' +
-    'CURDATE,CURRENT_DATE,CURRENT_TIME,CURRENT_TIMESTAMP,CURRENT_USER,' +
-    'CURTIME,DATE_ADD,DATE_FORMAT,DATE_SUB,DATEDIFF,DAY,DAYNAME,DAYOFMONTH,' +
-    'DAYOFWEEK,DAYOFYEAR,DECODE,DEGREES,DES_DECRYPT,DES_ENCRYPT,DIMENSION,' +
-    'DISJOINT,DISTANCE,ELT,ENCODE,ENCRYPT,ENDPOINT,ENVELOPE,EQUALS,EXP,' +
-    'EXPORT_SET,EXTERIORRING,EXTRACT,EXTRACTVALUE,FIELD,FIND_IN_SET,FLOOR,' +
-    'FORMAT,FOUND_ROWS,FROM_BASE64,FROM_DAYS,FROM_UNIXTIME,GEOMCOLLFROMTEXT,' +
-    'GEOMCOLLFROMWKB,GEOMETRYCOLLECTIONFROMTEXT,GEOMETRYCOLLECTIONFROMWKB,' +
-    'GEOMETRYFROMTEXT,GEOMETRYFROMWKB,GEOMETRYN,GEOMETRYTYPE,GEOMFROMTEXT,' +
-    'GEOMFROMWKB,GET_FORMAT,GET_LOCK,GLENGTH,GREATEST,GROUP_CONCAT,' +
-    'GTID_SUBSET,GTID_SUBTRACT,HEX,HOUR,IFNULL,INET_ATON,INET_NTOA,' +
-    'INET6_ATON,INET6_NTOA,INSTR,INTERIORRINGN,INTERSECTS,INTERVAL,' +
-    'IS_FREE_LOCK,IS_IPV4,IS_IPV4_COMPAT,IS_IPV4_MAPPED,IS_IPV6,IS_USED_LOCK,' +
-    'ISCLOSED,ISEMPTY,ISNULL,ISSIMPLE,JSON_APPEND,JSON_ARRAY,' +
-    'JSON_ARRAY_APPEND,JSON_ARRAY_INSERT,JSON_CONTAINS,JSON_CONTAINS_PATH,' +
-    'JSON_DEPTH,JSON_EXTRACT,JSON_INSERT,JSON_KEYS,JSON_LENGTH,JSON_MERGE,' +
-    'JSON_OBJECT,JSON_QUOTE,JSON_REMOVE,JSON_REPLACE,JSON_SEARCH,JSON_SET,' +
-    'JSON_TYPE,JSON_UNQUOTE,JSON_VALID,LAST_DAY,LAST_INSERT_ID,LCASE,LEAST,' +
-    'LEFT,LENGTH,LINEFROMTEXT,LINEFROMWKB,LINESTRINGFROMTEXT,' +
-    'LINESTRINGFROMWKB,LN,LOAD_FILE,LOCALTI,LOCALTIME,LOCALTIMESTAMP,LOCATE,' +
-    'LOG,LOG10,LOG2,LOWER,LPAD,LTRIM,MAKE_SET,MAKEDATE,MAKETIME,' +
-    'MASTER_POS_WAIT,MAX,MBRCONTAINS,MBRCOVEREDBY,MBRCOVERS,MBRDISJOINT,' +
-    'MBREQUAL,MBREQUALS,MBRINTERSECTS,MBROVERLAPS,MBRTOUCHES,MBRWITHIN,MD5,' +
-    'MICROSECOND,MID,MIN,MINUTE,MLINEFROMTEXT,MLINEFROMWKB,MONTH,' +
-    'MONTHNAME,MPOINTFROMTEXT,MPOINTFROMWKB,MPOLYFROMTEXT,MPOLYFROMWKB,' +
-    'MULTILINESTRINGFROMTEXT,MULTILINESTRINGFROMWKB,MULTIPOINTFROMTEXT,' +
-    'MULTIPOINTFROMWKB,MULTIPOLYGONFROMTEXT,MULTIPOLYGONFROMWKB,NAME_CONST,' +
-    'NOW,NULLIF,NUMGEOMETRIES,NUMINTERIORRINGS,NUMPOINTS,OCT,OCTET_LENGTH,' +
-    'OLD_PASSWORD,ORD,OVERLAPS,PASSWORD,PERIOD_ADD,PERIOD_DIFF,PI,' +
-    'POINTFROMTEXT,POINTFROMWKB,POINTN,POLYFROMTEXT,POLYFROMWKB,' +
-    'POLYGONFROMTEXT,POLYGONFROMWKB,POSITION,POW,POWER,QUARTER,QUOTE,RADIANS,' +
-    'RAND,RANDOM_BYTES,RELEASE_ALL_LOCKS,RELEASE_LOCK,REVERSE,RIGHT,ROUND,' +
-    'ROW_COUNT,RPAD,RTRIM,SEC_TO_TIME,SECOND,SESSION_USER,SHA,SHA1,SHA2,SIGN,' +
-    'SIN,SLEEP,SOUNDEX,SPACE,SQRT,SRID,ST_AREA,ST_ASBINARY,ST_ASGEOJSON,' +
-    'ST_ASTEXT,ST_ASWKB,ST_ASWKT,ST_BUFFER,ST_BUFFER_STRATEGY,ST_CENTROID,' +
-    'ST_CONTAINS,ST_CONVEXHULL,ST_CROSSES,ST_DIFFERENCE,ST_DIMENSION,' +
-    'ST_DISJOINT,ST_DISTANCE,ST_DISTANCE_SPHERE,ST_ENDPOINT,ST_ENVELOPE,' +
-    'ST_EQUALS,ST_EXTERIORRING,ST_GEOHASH,ST_GEOMCOLLFROMTEXT,' +
-    'ST_GEOMCOLLFROMTXT,ST_GEOMCOLLFROMWKB,ST_GEOMETRYCOLLECTIONFROMTEXT,' +
-    'ST_GEOMETRYCOLLECTIONFROMWKB,ST_GEOMETRYFROMTEXT,ST_GEOMETRYFROMWKB,' +
-    'ST_GEOMETRYN,ST_GEOMETRYTYPE,ST_GEOMFROMGEOJSON,ST_GEOMFROMTEXT,' +
-    'ST_GEOMFROMWKB,ST_INTERIORRINGN,ST_INTERSECTION,ST_INTERSECTS,' +
-    'ST_ISCLOSED,ST_ISEMPTY,ST_ISSIMPLE,ST_ISVALID,ST_LATFROMGEOHASH,' +
-    'ST_LENGTH,ST_LINEFROMTEXT,ST_LINEFROMWKB,ST_LINESTRINGFROMTEXT,' +
-    'ST_LINESTRINGFROMWKB,ST_LONGFROMGEOHASH,ST_MAKEENVELOPE,' +
-    'ST_MLINEFROMTEXT,ST_MLINEFROMWKB,ST_MPOINTFROMTEXT,ST_MPOINTFROMWKB,' +
-    'ST_MPOLYFROMTEXT,ST_MPOLYFROMWKB,ST_MULTILINESTRINGFROMTEXT,' +
-    'ST_MULTILINESTRINGFROMWKB,ST_MULTIPOINTFROMTEXT,ST_MULTIPOINTFROMWKB,' +
-    'ST_MULTIPOLYGONFROMTEXT,ST_MULTIPOLYGONFROMWKB,ST_NUMGEOMETRIES,' +
-    'ST_NUMINTERIORRING,ST_NUMINTERIORRINGS,ST_NUMPOINTS,ST_OVERLAPS,' +
-    'ST_POINTFROMGEOHASH,ST_POINTFROMTEXT,ST_POINTFROMWKB,ST_POINTN,' +
-    'ST_POLYFROMTEXT,ST_POLYFROMWKB,ST_POLYGONFROMTEXT,ST_POLYGONFROMWKB,' +
-    'ST_SIMPLIFY,ST_SRID,ST_STARTPOINT,ST_SYMDIFFERENCE,ST_TOUCHES,ST_UNION,' +
-    'ST_VALIDATE,ST_WITHIN,ST_X,ST_Y,STARTPOINT,STD,STDDEV,STDDEV_POP,' +
-    'STDDEV_SAMP,STR_TO_DATE,STRCMP,SUBDATE,SUBSTR,SUBSTRING,SUBSTRING_INDEX,' +
-    'SUBTIME,SUM,SYSDATE,SYSTEM_USER,TAN,TIME_FORMAT,TIME_TO_SEC,TIMEDIFF,' +
-    'TIMESTAMPADD,TIMESTAMPDIFF,TO_BASE64,TO_DAYS,TO_SECONDS,TOUCHES,TRIM,' +
-    'TRUNCATE,UCASE,UNCOMPRESS,UNCOMPRESSED_LENGTH,UNHEX,UNIX_TIMESTAMP,' +
-    'UPDATEXML,UPPER,USER,UTC_DATE,UTC_TIME,UTC_TIMESTAMP,UUID,UUID_SHORT,' +
-    'VALIDATE_PASSWORD_STRENGTH,VAR_POP,VAR_SAMP,VARIANCE,VERSION,' +
-    'WAIT_FOR_EXECUTED_GTID_SET,WAIT_UNTIL_SQL_THREAD_AFTER_GTIDS,WEEK,' +
-    'WEEKDAY,WEEKOFYEAR,WEIGHT_STRING,WITHIN,X,Y,YEARWEEK';
+    'ABS,ACOS,ADDDATE,ADDTIME,AES_DECRYPT,AES_ENCRYPT,AREA,ASBINARY,ASCII' +
+    'ASIN,ASTEXT,ASWKB,ASWKT,ATAN,ATAN2,AVG,BENCHMARK,BIN,BINLOG_GTID_POS' +
+    'BIT_AND,BIT_COUNT,BIT_LENGTH,BIT_OR,BIT_XOR,BOUNDARY,BUFFER,CAST,CEIL' +
+    'CEILING,CENTROID,CHARACTER_LENGTH,CHAR_LENGTH,CHR,COERCIBILITY' +
+    'COLUMN_ADD,COLUMN_CHECK,COLUMN_CREATE,COLUMN_DELETE,COLUMN_EXISTS' +
+    'COLUMN_GET,COLUMN_JSON,COLUMN_LIST,COMPRESS,CONCAT,CONCAT_WS' +
+    'CONNECTION_ID,CONV,CONVERT_TZ,CONVEXHULL,COS,COT,COUNT,CRC32,CROSSES' +
+    'CUME_DIST,CURDATE,CURRENT_DATE,CURRENT_ROLE,CURRENT_TIME' +
+    'CURRENT_TIMESTAMP,CURRENT_USER,CURTIME,DATEDIFF,DATE_ADD,DATE_FORMAT' +
+    'DATE_SUB,DAY,DAYNAME,DAYOFMONTH,DAYOFWEEK,DAYOFYEAR,DECODE' +
+    'DECODE_HISTOGRAM,DEGREES,DENSE_RANK,DES_DECRYPT,DES_ENCRYPT,DIMENSION' +
+    'DISJOINT,ELT,ENCODE,ENCRYPT,ENDPOINT,ENVELOPE,EQUALS,EXP,EXPORT_SET' +
+    'EXTERIORRING,EXTRACT,EXTRACTVALUE,FIELD,FIND_IN_SET,FIRST_VALUE,FLOOR' +
+    'FORMAT,FOUND_ROWS,FROM_BASE64,FROM_DAYS,FROM_UNIXTIME,GEOMCOLLFROMTEXT' +
+    'GEOMCOLLFROMWKB,GEOMETRYCOLLECTIONFROMTEXT,GEOMETRYCOLLECTIONFROMWKB' +
+    'GEOMETRYFROMTEXT,GEOMETRYFROMWKB,GEOMETRYN,GEOMETRYTYPE,GEOMFROMTEXT' +
+    'GEOMFROMWKB,GET_FORMAT,GET_LOCK,GLENGTH,GROUP_CONCAT,HELP_DATE,HEX' +
+    'HOUR,IFNULL,INET6_ATON,INET6_NTOA,INET_ATON,INET_NTOA,INSTR' +
+    'INTERIORRINGN,INTERSECTS,ISCLOSED,ISEMPTY,ISRING,ISSIMPLE,IS_FREE_LOCK' +
+    'IS_IPV4,IS_IPV4_COMPAT,IS_IPV4_MAPPED,IS_IPV6,IS_USED_LOCK,JSON_ARRAY' +
+    'JSON_ARRAY_APPEND,JSON_ARRAY_INSERT,JSON_COMPACT,JSON_CONTAINS' +
+    'JSON_CONTAINS_PATH,JSON_DEPTH,JSON_DETAILED,JSON_EXISTS,JSON_EXTRACT' +
+    'JSON_INSERT,JSON_KEYS,JSON_LENGTH,JSON_LOOSE,JSON_MERGE,JSON_OBJECT' +
+    'JSON_QUERY,JSON_QUOTE,JSON_REMOVE,JSON_REPLACE,JSON_SEARCH,JSON_SET' +
+    'JSON_TYPE,JSON_UNQUOTE,JSON_VALID,JSON_VALUE,LABELS,LAG,LASTVAL' +
+    'LAST_DAY,LAST_INSERT_ID,LAST_VALUE,LCASE,LEFT,LENGTH,LENGTHB' +
+    'LINEFROMTEXT,LINEFROMWKB,LINESTRINGFROMTEXT,LINESTRINGFROMWKB,LN' +
+    'LOAD_FILE,LOCALTIME,LOCALTIMESTAMP,LOCATE,LOG,LOG10,LOG2,LOWER,LPAD' +
+    'LTRIM,MAKEDATE,MAKETIME,MAKE_SET,MASTER_GTID_WAIT,MASTER_POS_WAIT,MAX' +
+    'MBRCONTAINS,MBRDISJOINT,MBREQUAL,MBRINTERSECTS,MBROVERLAPS,MBRTOUCHES' +
+    'MBRWITHIN,MD5,MEDIAN,MICROSECOND,MID,MIN,MINUTE,MLINEFROMTEXT' +
+    'MLINEFROMWKB,MONTH,MONTHNAME,MPOINTFROMTEXT,MPOINTFROMWKB' +
+    'MPOLYFROMTEXT,MPOLYFROMWKB,MULTILINESTRINGFROMTEXT' +
+    'MULTILINESTRINGFROMWKB,MULTIPOINTFROMTEXT,MULTIPOINTFROMWKB' +
+    'MULTIPOLYGONFROMTEXT,MULTIPOLYGONFROMWKB,NAME_CONST,NEXTVAL,NOW' +
+    'NTH_VALUE,NTILE,NULLIF,NUMGEOMETRIES,NUMINTERIORRINGS,NUMPOINTS,OCT' +
+    'OCTET_LENGTH,OLD_PASSWORD,ORD,OVERLAPS,PARENTHESES,PASSWORD' +
+    'PERCENTILE_CONT,PERCENTILE_DISC,PERCENT_RANK,PERIOD_ADD,PERIOD_DIFF,PI' +
+    'POINTFROMTEXT,POINTFROMWKB,POINTN,POINTONSURFACE,POLYFROMTEXT' +
+    'POLYFROMWKB,POLYGONFROMTEXT,POLYGONFROMWKB,POSITION,POW,POWER,QUARTER' +
+    'QUOTE,RADIANS,RAND,RANK,REGEXP_INSTR,REGEXP_REPLACE,REGEXP_SUBSTR' +
+    'RELEASE_LOCK,REVERSE,RIGHT,ROUND,ROW_COUNT,ROW_NUMBER,RPAD,RTRIM' +
+    'SECOND,SEC_TO_TIME,SESSION_USER,SETVAL(),SHA1,SHA2,SIGN,SIN,SLEEP' +
+    'SOUNDEX,SPACE,SPIDER_BG_DIRECT_SQL,SPIDER_COPY_TABLES' +
+    'SPIDER_DIRECT_SQL,SPIDER_FLUSH_TABLE_MON_CACHE,SQRT,SRID,STARTPOINT' +
+    'STD,STDDEV,STDDEV_POP,STDDEV_SAMP,STRCMP,STR_TO_DATE,ST_AREA' +
+    'ST_ASBINARY,ST_ASTEXT,ST_ASWKB,ST_ASWKT,ST_BOUNDARY,ST_BUFFER' +
+    'ST_CENTROID,ST_CONTAINS,ST_CONVEXHULL,ST_CROSSES,ST_DIFFERENCE' +
+    'ST_DIMENSION,ST_DISJOINT,ST_DISTANCE,ST_ENDPOINT,ST_ENVELOPE,ST_EQUALS' +
+    'ST_EXTERIORRING,ST_GEOMCOLLFROMTEXT,ST_GEOMCOLLFROMWKB' +
+    'ST_GEOMETRYCOLLECTIONFROMTEXT,ST_GEOMETRYCOLLECTIONFROMWKB' +
+    'ST_GEOMETRYFROMTEXT,ST_GEOMETRYFROMWKB,ST_GEOMETRYN,ST_GEOMETRYTYPE' +
+    'ST_GEOMFROMTEXT,ST_GEOMFROMWKB,ST_INTERIORRINGN,ST_INTERSECTION' +
+    'ST_INTERSECTS,ST_ISCLOSED,ST_ISEMPTY,ST_ISRING,ST_ISSIMPLE,ST_LENGTH' +
+    'ST_LINEFROMTEXT,ST_LINEFROMWKB,ST_LINESTRINGFROMTEXT' +
+    'ST_LINESTRINGFROMWKB,ST_NUMGEOMETRIES,ST_NUMINTERIORRINGS,ST_NUMPOINTS' +
+    'ST_OVERLAPS,ST_POINTFROMTEXT,ST_POINTFROMWKB,ST_POINTN' +
+    'ST_POINTONSURFACE,ST_POLYFROMTEXT,ST_POLYFROMWKB,ST_POLYGONFROMTEXT' +
+    'ST_POLYGONFROMWKB,ST_RELATE,ST_SRID,ST_SYMDIFFERENCE,ST_TOUCHES' +
+    'ST_UNION,ST_WITHIN,ST_X,ST_Y,SUBDATE,SUBSTR,SUBSTRING,SUBSTRING_INDEX' +
+    'SUBTIME,SUM,SYSDATE,SYSTEM_USER,TAN,TIMEDIFF,TIMESTAMPADD' +
+    'TIMESTAMPDIFF,TIME_FORMAT,TIME_TO_SEC,TOUCHES,TO_BASE64,TO_DAYS' +
+    'TO_SECONDS,TRIM,TRUNCATE,UCASE,UNCOMPRESS,UNCOMPRESSED_LENGTH,UNHEX' +
+    'UNIX_TIMESTAMP,UPDATEXML,UPPER,USER,UTC_DATE,UTC_TIME,UTC_TIMESTAMP' +
+    'UUID,UUID_SHORT,VARIANCE,VAR_POP,VAR_SAMP,VERSION,WEEK,WEEKDAY' +
+    'WEEKOFYEAR,WEIGHT_STRING,WITHIN,X,Y,YEARWEEK';
 
 //---Ingres---------------------------------------------------------------------
   // keywords

commit 30892528984d2c2d83c43b2544e98ccb177f74fd

Fix missing commas in SynEdit's list of MySQL functions, leading to non-highlighted words. Broken in 7e4094d37b01c3356c6b8e65fed14fb97ab78b4b

components/synedit/Source/SynHighlighterSQL.pas
@@ -1097,71 +1097,71 @@ const
   // functions
   // Taken from MariaDB 10.5.5 help tables
   MySQLFunctions: UnicodeString =
-    'ABS,ACOS,ADDDATE,ADDTIME,AES_DECRYPT,AES_ENCRYPT,AREA,ASBINARY,ASCII' +
-    'ASIN,ASTEXT,ASWKB,ASWKT,ATAN,ATAN2,AVG,BENCHMARK,BIN,BINLOG_GTID_POS' +
-    'BIT_AND,BIT_COUNT,BIT_LENGTH,BIT_OR,BIT_XOR,BOUNDARY,BUFFER,CAST,CEIL' +
-    'CEILING,CENTROID,CHARACTER_LENGTH,CHAR_LENGTH,CHR,COERCIBILITY' +
-    'COLUMN_ADD,COLUMN_CHECK,COLUMN_CREATE,COLUMN_DELETE,COLUMN_EXISTS' +
-    'COLUMN_GET,COLUMN_JSON,COLUMN_LIST,COMPRESS,CONCAT,CONCAT_WS' +
-    'CONNECTION_ID,CONV,CONVERT_TZ,CONVEXHULL,COS,COT,COUNT,CRC32,CROSSES' +
-    'CUME_DIST,CURDATE,CURRENT_DATE,CURRENT_ROLE,CURRENT_TIME' +
-    'CURRENT_TIMESTAMP,CURRENT_USER,CURTIME,DATEDIFF,DATE_ADD,DATE_FORMAT' +
-    'DATE_SUB,DAY,DAYNAME,DAYOFMONTH,DAYOFWEEK,DAYOFYEAR,DECODE' +
-    'DECODE_HISTOGRAM,DEGREES,DENSE_RANK,DES_DECRYPT,DES_ENCRYPT,DIMENSION' +
-    'DISJOINT,ELT,ENCODE,ENCRYPT,ENDPOINT,ENVELOPE,EQUALS,EXP,EXPORT_SET' +
-    'EXTERIORRING,EXTRACT,EXTRACTVALUE,FIELD,FIND_IN_SET,FIRST_VALUE,FLOOR' +
-    'FORMAT,FOUND_ROWS,FROM_BASE64,FROM_DAYS,FROM_UNIXTIME,GEOMCOLLFROMTEXT' +
-    'GEOMCOLLFROMWKB,GEOMETRYCOLLECTIONFROMTEXT,GEOMETRYCOLLECTIONFROMWKB' +
-    'GEOMETRYFROMTEXT,GEOMETRYFROMWKB,GEOMETRYN,GEOMETRYTYPE,GEOMFROMTEXT' +
-    'GEOMFROMWKB,GET_FORMAT,GET_LOCK,GLENGTH,GROUP_CONCAT,HELP_DATE,HEX' +
-    'HOUR,IFNULL,INET6_ATON,INET6_NTOA,INET_ATON,INET_NTOA,INSTR' +
-    'INTERIORRINGN,INTERSECTS,ISCLOSED,ISEMPTY,ISRING,ISSIMPLE,IS_FREE_LOCK' +
-    'IS_IPV4,IS_IPV4_COMPAT,IS_IPV4_MAPPED,IS_IPV6,IS_USED_LOCK,JSON_ARRAY' +
-    'JSON_ARRAY_APPEND,JSON_ARRAY_INSERT,JSON_COMPACT,JSON_CONTAINS' +
-    'JSON_CONTAINS_PATH,JSON_DEPTH,JSON_DETAILED,JSON_EXISTS,JSON_EXTRACT' +
-    'JSON_INSERT,JSON_KEYS,JSON_LENGTH,JSON_LOOSE,JSON_MERGE,JSON_OBJECT' +
-    'JSON_QUERY,JSON_QUOTE,JSON_REMOVE,JSON_REPLACE,JSON_SEARCH,JSON_SET' +
-    'JSON_TYPE,JSON_UNQUOTE,JSON_VALID,JSON_VALUE,LABELS,LAG,LASTVAL' +
-    'LAST_DAY,LAST_INSERT_ID,LAST_VALUE,LCASE,LEFT,LENGTH,LENGTHB' +
-    'LINEFROMTEXT,LINEFROMWKB,LINESTRINGFROMTEXT,LINESTRINGFROMWKB,LN' +
-    'LOAD_FILE,LOCALTIME,LOCALTIMESTAMP,LOCATE,LOG,LOG10,LOG2,LOWER,LPAD' +
-    'LTRIM,MAKEDATE,MAKETIME,MAKE_SET,MASTER_GTID_WAIT,MASTER_POS_WAIT,MAX' +
-    'MBRCONTAINS,MBRDISJOINT,MBREQUAL,MBRINTERSECTS,MBROVERLAPS,MBRTOUCHES' +
-    'MBRWITHIN,MD5,MEDIAN,MICROSECOND,MID,MIN,MINUTE,MLINEFROMTEXT' +
-    'MLINEFROMWKB,MONTH,MONTHNAME,MPOINTFROMTEXT,MPOINTFROMWKB' +
-    'MPOLYFROMTEXT,MPOLYFROMWKB,MULTILINESTRINGFROMTEXT' +
-    'MULTILINESTRINGFROMWKB,MULTIPOINTFROMTEXT,MULTIPOINTFROMWKB' +
-    'MULTIPOLYGONFROMTEXT,MULTIPOLYGONFROMWKB,NAME_CONST,NEXTVAL,NOW' +
-    'NTH_VALUE,NTILE,NULLIF,NUMGEOMETRIES,NUMINTERIORRINGS,NUMPOINTS,OCT' +
-    'OCTET_LENGTH,OLD_PASSWORD,ORD,OVERLAPS,PARENTHESES,PASSWORD' +
-    'PERCENTILE_CONT,PERCENTILE_DISC,PERCENT_RANK,PERIOD_ADD,PERIOD_DIFF,PI' +
-    'POINTFROMTEXT,POINTFROMWKB,POINTN,POINTONSURFACE,POLYFROMTEXT' +
-    'POLYFROMWKB,POLYGONFROMTEXT,POLYGONFROMWKB,POSITION,POW,POWER,QUARTER' +
-    'QUOTE,RADIANS,RAND,RANK,REGEXP_INSTR,REGEXP_REPLACE,REGEXP_SUBSTR' +
-    'RELEASE_LOCK,REVERSE,RIGHT,ROUND,ROW_COUNT,ROW_NUMBER,RPAD,RTRIM' +
-    'SECOND,SEC_TO_TIME,SESSION_USER,SETVAL(),SHA1,SHA2,SIGN,SIN,SLEEP' +
-    'SOUNDEX,SPACE,SPIDER_BG_DIRECT_SQL,SPIDER_COPY_TABLES' +
-    'SPIDER_DIRECT_SQL,SPIDER_FLUSH_TABLE_MON_CACHE,SQRT,SRID,STARTPOINT' +
-    'STD,STDDEV,STDDEV_POP,STDDEV_SAMP,STRCMP,STR_TO_DATE,ST_AREA' +
-    'ST_ASBINARY,ST_ASTEXT,ST_ASWKB,ST_ASWKT,ST_BOUNDARY,ST_BUFFER' +
-    'ST_CENTROID,ST_CONTAINS,ST_CONVEXHULL,ST_CROSSES,ST_DIFFERENCE' +
-    'ST_DIMENSION,ST_DISJOINT,ST_DISTANCE,ST_ENDPOINT,ST_ENVELOPE,ST_EQUALS' +
-    'ST_EXTERIORRING,ST_GEOMCOLLFROMTEXT,ST_GEOMCOLLFROMWKB' +
-    'ST_GEOMETRYCOLLECTIONFROMTEXT,ST_GEOMETRYCOLLECTIONFROMWKB' +
-    'ST_GEOMETRYFROMTEXT,ST_GEOMETRYFROMWKB,ST_GEOMETRYN,ST_GEOMETRYTYPE' +
-    'ST_GEOMFROMTEXT,ST_GEOMFROMWKB,ST_INTERIORRINGN,ST_INTERSECTION' +
-    'ST_INTERSECTS,ST_ISCLOSED,ST_ISEMPTY,ST_ISRING,ST_ISSIMPLE,ST_LENGTH' +
-    'ST_LINEFROMTEXT,ST_LINEFROMWKB,ST_LINESTRINGFROMTEXT' +
-    'ST_LINESTRINGFROMWKB,ST_NUMGEOMETRIES,ST_NUMINTERIORRINGS,ST_NUMPOINTS' +
-    'ST_OVERLAPS,ST_POINTFROMTEXT,ST_POINTFROMWKB,ST_POINTN' +
-    'ST_POINTONSURFACE,ST_POLYFROMTEXT,ST_POLYFROMWKB,ST_POLYGONFROMTEXT' +
-    'ST_POLYGONFROMWKB,ST_RELATE,ST_SRID,ST_SYMDIFFERENCE,ST_TOUCHES' +
-    'ST_UNION,ST_WITHIN,ST_X,ST_Y,SUBDATE,SUBSTR,SUBSTRING,SUBSTRING_INDEX' +
-    'SUBTIME,SUM,SYSDATE,SYSTEM_USER,TAN,TIMEDIFF,TIMESTAMPADD' +
-    'TIMESTAMPDIFF,TIME_FORMAT,TIME_TO_SEC,TOUCHES,TO_BASE64,TO_DAYS' +
-    'TO_SECONDS,TRIM,TRUNCATE,UCASE,UNCOMPRESS,UNCOMPRESSED_LENGTH,UNHEX' +
-    'UNIX_TIMESTAMP,UPDATEXML,UPPER,USER,UTC_DATE,UTC_TIME,UTC_TIMESTAMP' +
-    'UUID,UUID_SHORT,VARIANCE,VAR_POP,VAR_SAMP,VERSION,WEEK,WEEKDAY' +
+    'ABS,ACOS,ADDDATE,ADDTIME,AES_DECRYPT,AES_ENCRYPT,AREA,ASBINARY,ASCII,' +
+    'ASIN,ASTEXT,ASWKB,ASWKT,ATAN,ATAN2,AVG,BENCHMARK,BIN,BINLOG_GTID_POS,' +
+    'BIT_AND,BIT_COUNT,BIT_LENGTH,BIT_OR,BIT_XOR,BOUNDARY,BUFFER,CAST,CEIL,' +
+    'CEILING,CENTROID,CHARACTER_LENGTH,CHAR_LENGTH,CHR,COERCIBILITY,' +
+    'COLUMN_ADD,COLUMN_CHECK,COLUMN_CREATE,COLUMN_DELETE,COLUMN_EXISTS,' +
+    'COLUMN_GET,COLUMN_JSON,COLUMN_LIST,COMPRESS,CONCAT,CONCAT_WS,' +
+    'CONNECTION_ID,CONV,CONVERT_TZ,CONVEXHULL,COS,COT,COUNT,CRC32,CROSSES,' +
+    'CUME_DIST,CURDATE,CURRENT_DATE,CURRENT_ROLE,CURRENT_TIME,' +
+    'CURRENT_TIMESTAMP,CURRENT_USER,CURTIME,DATEDIFF,DATE_ADD,DATE_FORMAT,' +
+    'DATE_SUB,DAY,DAYNAME,DAYOFMONTH,DAYOFWEEK,DAYOFYEAR,DECODE,' +
+    'DECODE_HISTOGRAM,DEGREES,DENSE_RANK,DES_DECRYPT,DES_ENCRYPT,DIMENSION,' +
+    'DISJOINT,ELT,ENCODE,ENCRYPT,ENDPOINT,ENVELOPE,EQUALS,EXP,EXPORT_SET,' +
+    'EXTERIORRING,EXTRACT,EXTRACTVALUE,FIELD,FIND_IN_SET,FIRST_VALUE,FLOOR,' +
+    'FORMAT,FOUND_ROWS,FROM_BASE64,FROM_DAYS,FROM_UNIXTIME,GEOMCOLLFROMTEXT,' +
+    'GEOMCOLLFROMWKB,GEOMETRYCOLLECTIONFROMTEXT,GEOMETRYCOLLECTIONFROMWKB,' +
+    'GEOMETRYFROMTEXT,GEOMETRYFROMWKB,GEOMETRYN,GEOMETRYTYPE,GEOMFROMTEXT,' +
+    'GEOMFROMWKB,GET_FORMAT,GET_LOCK,GLENGTH,GROUP_CONCAT,HELP_DATE,HEX,' +
+    'HOUR,IFNULL,INET6_ATON,INET6_NTOA,INET_ATON,INET_NTOA,INSTR,' +
+    'INTERIORRINGN,INTERSECTS,ISCLOSED,ISEMPTY,ISRING,ISSIMPLE,IS_FREE_LOCK,' +
+    'IS_IPV4,IS_IPV4_COMPAT,IS_IPV4_MAPPED,IS_IPV6,IS_USED_LOCK,JSON_ARRAY,' +
+    'JSON_ARRAY_APPEND,JSON_ARRAY_INSERT,JSON_COMPACT,JSON_CONTAINS,' +
+    'JSON_CONTAINS_PATH,JSON_DEPTH,JSON_DETAILED,JSON_EXISTS,JSON_EXTRACT,' +
+    'JSON_INSERT,JSON_KEYS,JSON_LENGTH,JSON_LOOSE,JSON_MERGE,JSON_OBJECT,' +
+    'JSON_QUERY,JSON_QUOTE,JSON_REMOVE,JSON_REPLACE,JSON_SEARCH,JSON_SET,' +
+    'JSON_TYPE,JSON_UNQUOTE,JSON_VALID,JSON_VALUE,LABELS,LAG,LASTVAL,' +
+    'LAST_DAY,LAST_INSERT_ID,LAST_VALUE,LCASE,LEFT,LENGTH,LENGTHB,' +
+    'LINEFROMTEXT,LINEFROMWKB,LINESTRINGFROMTEXT,LINESTRINGFROMWKB,LN,' +
+    'LOAD_FILE,LOCALTIME,LOCALTIMESTAMP,LOCATE,LOG,LOG10,LOG2,LOWER,LPAD,' +
+    'LTRIM,MAKEDATE,MAKETIME,MAKE_SET,MASTER_GTID_WAIT,MASTER_POS_WAIT,MAX,' +
+    'MBRCONTAINS,MBRDISJOINT,MBREQUAL,MBRINTERSECTS,MBROVERLAPS,MBRTOUCHES,' +
+    'MBRWITHIN,MD5,MEDIAN,MICROSECOND,MID,MIN,MINUTE,MLINEFROMTEXT,' +
+    'MLINEFROMWKB,MONTH,MONTHNAME,MPOINTFROMTEXT,MPOINTFROMWKB,' +
+    'MPOLYFROMTEXT,MPOLYFROMWKB,MULTILINESTRINGFROMTEXT,' +
+    'MULTILINESTRINGFROMWKB,MULTIPOINTFROMTEXT,MULTIPOINTFROMWKB,' +
+    'MULTIPOLYGONFROMTEXT,MULTIPOLYGONFROMWKB,NAME_CONST,NEXTVAL,NOW,' +
+    'NTH_VALUE,NTILE,NULLIF,NUMGEOMETRIES,NUMINTERIORRINGS,NUMPOINTS,OCT,' +
+    'OCTET_LENGTH,OLD_PASSWORD,ORD,OVERLAPS,PARENTHESES,PASSWORD,' +
+    'PERCENTILE_CONT,PERCENTILE_DISC,PERCENT_RANK,PERIOD_ADD,PERIOD_DIFF,PI,' +
+    'POINTFROMTEXT,POINTFROMWKB,POINTN,POINTONSURFACE,POLYFROMTEXT,' +
+    'POLYFROMWKB,POLYGONFROMTEXT,POLYGONFROMWKB,POSITION,POW,POWER,QUARTER,' +
+    'QUOTE,RADIANS,RAND,RANK,REGEXP_INSTR,REGEXP_REPLACE,REGEXP_SUBSTR,' +
+    'RELEASE_LOCK,REVERSE,RIGHT,ROUND,ROW_COUNT,ROW_NUMBER,RPAD,RTRIM,' +
+    'SECOND,SEC_TO_TIME,SESSION_USER,SETVAL(),SHA1,SHA2,SIGN,SIN,SLEEP,' +
+    'SOUNDEX,SPACE,SPIDER_BG_DIRECT_SQL,SPIDER_COPY_TABLES,' +
+    'SPIDER_DIRECT_SQL,SPIDER_FLUSH_TABLE_MON_CACHE,SQRT,SRID,STARTPOINT,' +
+    'STD,STDDEV,STDDEV_POP,STDDEV_SAMP,STRCMP,STR_TO_DATE,ST_AREA,' +
+    'ST_ASBINARY,ST_ASTEXT,ST_ASWKB,ST_ASWKT,ST_BOUNDARY,ST_BUFFER,' +
+    'ST_CENTROID,ST_CONTAINS,ST_CONVEXHULL,ST_CROSSES,ST_DIFFERENCE,' +
+    'ST_DIMENSION,ST_DISJOINT,ST_DISTANCE,ST_ENDPOINT,ST_ENVELOPE,ST_EQUALS,' +
+    'ST_EXTERIORRING,ST_GEOMCOLLFROMTEXT,ST_GEOMCOLLFROMWKB,' +
+    'ST_GEOMETRYCOLLECTIONFROMTEXT,ST_GEOMETRYCOLLECTIONFROMWKB,' +
+    'ST_GEOMETRYFROMTEXT,ST_GEOMETRYFROMWKB,ST_GEOMETRYN,ST_GEOMETRYTYPE,' +
+    'ST_GEOMFROMTEXT,ST_GEOMFROMWKB,ST_INTERIORRINGN,ST_INTERSECTION,' +
+    'ST_INTERSECTS,ST_ISCLOSED,ST_ISEMPTY,ST_ISRING,ST_ISSIMPLE,ST_LENGTH,' +
+    'ST_LINEFROMTEXT,ST_LINEFROMWKB,ST_LINESTRINGFROMTEXT,' +
+    'ST_LINESTRINGFROMWKB,ST_NUMGEOMETRIES,ST_NUMINTERIORRINGS,ST_NUMPOINTS,' +
+    'ST_OVERLAPS,ST_POINTFROMTEXT,ST_POINTFROMWKB,ST_POINTN,' +
+    'ST_POINTONSURFACE,ST_POLYFROMTEXT,ST_POLYFROMWKB,ST_POLYGONFROMTEXT,' +
+    'ST_POLYGONFROMWKB,ST_RELATE,ST_SRID,ST_SYMDIFFERENCE,ST_TOUCHES,' +
+    'ST_UNION,ST_WITHIN,ST_X,ST_Y,SUBDATE,SUBSTR,SUBSTRING,SUBSTRING_INDEX,' +
+    'SUBTIME,SUM,SYSDATE,SYSTEM_USER,TAN,TIMEDIFF,TIMESTAMPADD,' +
+    'TIMESTAMPDIFF,TIME_FORMAT,TIME_TO_SEC,TOUCHES,TO_BASE64,TO_DAYS,' +
+    'TO_SECONDS,TRIM,TRUNCATE,UCASE,UNCOMPRESS,UNCOMPRESSED_LENGTH,UNHEX,' +
+    'UNIX_TIMESTAMP,UPDATEXML,UPPER,USER,UTC_DATE,UTC_TIME,UTC_TIMESTAMP,' +
+    'UUID,UUID_SHORT,VARIANCE,VAR_POP,VAR_SAMP,VERSION,WEEK,WEEKDAY,' +
     'WEEKOFYEAR,WEIGHT_STRING,WITHIN,X,Y,YEARWEEK';
 
 //---Ingres---------------------------------------------------------------------