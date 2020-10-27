HeidiSQL/HeidiSQL

commit b22c3fd81549edba525e526cb6429aad2fa1faf3

Dynamic default value for library session setting, depending on its network type. Closes #1010

source/apphelpers.pas
@@ -3518,7 +3518,7 @@ begin
   InitSetting(asWindowsAuth,                      'WindowsAuth',                           0, False, '', True);
   InitSetting(asLoginPrompt,                      'LoginPrompt',                           0, False, '', True);
   InitSetting(asPort,                             'Port',                                  0, False, '', True);
-  InitSetting(asLibrary,                          'Library',                               0, False, 'libmariadb.dll', True);
+  InitSetting(asLibrary,                          'Library',                               0, False, ''); // Gets its default in TConnectionParameters.Create
   InitSetting(asAllProviders,                     'AllProviders',                          0, False);
   InitSetting(asPlinkExecutable,                  'PlinkExecutable',                       0, False, 'plink.exe');
   InitSetting(asSSHtunnelHost,                    'SSHtunnelHost',                         0, False, '', True);

source/dbconnection.pas
@@ -1293,7 +1293,7 @@ begin
     FPort := MakeInt(AppSettings.ReadString(asPort));
     FCompressed := AppSettings.ReadBool(asCompressed);
     FAllDatabases := AppSettings.ReadString(asDatabases);
-    FLibraryOrProvider := AppSettings.ReadString(asLibrary);
+    FLibraryOrProvider := AppSettings.ReadString(asLibrary, '', DefaultLibrary);
     FComment := AppSettings.ReadString(asComment);
 
     FSSHHost := AppSettings.ReadString(asSSHtunnelHost);

commit d8cf039d98ce12df2baefd9da5731952ab6af6ca

Emergency fix: Turn asLibrary again into a session based setting. Broken in commit b22c3fd81549edba525e526cb6429aad2fa1faf3. Closes #1199

source/apphelpers.pas
@@ -3518,7 +3518,7 @@ begin
   InitSetting(asWindowsAuth,                      'WindowsAuth',                           0, False, '', True);
   InitSetting(asLoginPrompt,                      'LoginPrompt',                           0, False, '', True);
   InitSetting(asPort,                             'Port',                                  0, False, '', True);
-  InitSetting(asLibrary,                          'Library',                               0, False, ''); // Gets its default in TConnectionParameters.Create
+  InitSetting(asLibrary,                          'Library',                               0, False, '', True); // Gets its default in TConnectionParameters.Create
   InitSetting(asAllProviders,                     'AllProviders',                          0, False);
   InitSetting(asPlinkExecutable,                  'PlinkExecutable',                       0, False, 'plink.exe');
   InitSetting(asSSHtunnelHost,                    'SSHtunnelHost',                         0, False, '', True);