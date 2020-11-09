66c50ce79262c7bbcb6868d0285f24fc6ce0c0cb
Ansgar Becker <anse@heidisql.com>
2020-11-09 18:08:06 +0100

    Tab restoring: avoid restoring tab file only if the owner is a *HeidiSQL* process. Fixes non-restored tabs when an old Heidi process id is re-assigned to another application. Happened here at least one time.

--- source/main.pas
+++ source/main.pas
@@ -2361,6 +2361,7 @@ var
   pid: Cardinal;
   EditorHeight, HelpersWidth: Integer;
   BindParams: String;
+  ExeNameExpression: String;
 begin
   // Restore query tab setup from tabs.ini
   Result := True;
@@ -2371,6 +2372,11 @@ begin
 
     Sections := TStringList.Create;
     TabsIni.ReadSections(Sections);
+    ExeNameExpression := '(' +
+      QuoteRegExprMetaChars(ExtractFileName(Application.ExeName)) +
+      '|' +
+      QuoteRegExprMetaChars(APPNAME) +
+      ')';
 
     for Section in Sections do begin
 
@@ -2381,9 +2387,11 @@ begin
       HelpersWidth := TabsIni.ReadInteger(Section, 'HelpersWidth', 0);
       BindParams := TabsIni.ReadString(Section, 'BindParams', '');
 
-      // Don't restore this tab if it belongs to a different running process
-      if (pid > 0) and (pid <> GetCurrentProcessId) and ProcessExists(pid) then
+      // Don't restore this tab if it belongs to a different running Heidi process
+      if (pid > 0) and (pid <> GetCurrentProcessId) and ProcessExists(pid, ExeNameExpression) then begin
+        LogSQL(IfThen(BackupFilename.IsEmpty, Filename, BackupFilename)+' loaded in process #'+pid.ToString);
         Continue;
+      end;
 
       // Either we have a backup file, or a user stored file.
       // Both of them may not exist.
