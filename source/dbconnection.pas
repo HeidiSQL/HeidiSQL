HeidiSQL/HeidiSQL

commit b9b5976413dc6ff35e9e947816cc7264161b88f1

Fix slow data exports of large MS SQL tables, by introducing ORDER BY .. OFFSET .. FETCH clauses in SELECT's. Reduces the chunks to ~10000 rows. Larger results seem to take more time for iterating through rows. Closes #343

source/dbconnection.pas
@@ -6164,6 +6164,10 @@ begin
         obj.NodeType := lntTrigger
       else if (tp = 'FN') or (tp = 'TF') or (tp = 'IF') then
         obj.NodeType := lntFunction;
+      // Set reasonable default value for calculation of export chunks. See #343
+      // OFFSET..FETCH supported from v11.0/2012
+      if ServerVersionInt >= 1100 then
+        obj.AvgRowLen := 10*SIZE_KB;
       Results.Next;
     end;
     FreeAndNil(Results);
@@ -6596,9 +6600,18 @@ begin
         // TOP(x) clause for UPDATES + DELETES introduced in MSSQL 2005
         if ServerVersionInt >= 900 then
           Result := Result + 'TOP('+IntToStr(Limit)+') ';
-      end else if QueryType = 'SELECT' then
-        Result := Result + 'TOP '+IntToStr(Limit)+' ';
-      Result := Result + QueryBody;
+        Result := Result + QueryBody;
+      end else if QueryType = 'SELECT' then begin
+        if ServerVersionInt >= 1100 then begin
+          Result := Result + QueryBody;
+          if not ContainsText(Result, ' ORDER BY ') then
+            Result := Result + ' ORDER BY 1'; // mandatory for using with OFFSET/FETCH
+          Result := Result + ' OFFSET '+Offset.ToString+' ROWS FETCH NEXT '+Limit.ToString+' ROWS ONLY';
+        end else begin
+          // OFFSET not supported in < 2012
+          Result := Result + 'TOP ' + IntToStr(Limit) + ' ' + QueryBody;
+        end;
+      end;
     end;
     ngMySQL: begin
       Result := Result + QueryBody + ' LIMIT ';

commit 2ded25f76c75e95dc43ff919bd001d0995107a69

Fix empty query body when deleting grid rows in MSSQL mode, broken in b9b5976413dc6ff35e9e947816cc7264161b88f1. Closes #1294

source/dbconnection.pas
@@ -6696,7 +6696,7 @@ begin
   Result := QueryType + ' ';
   case FParameters.NetTypeGroup of
     ngMSSQL: begin
-      if QueryType = 'UPDATE' then begin
+      if (QueryType = 'UPDATE') or (QueryType = 'DELETE') then begin
         // TOP(x) clause for UPDATES + DELETES introduced in MSSQL 2005
         if ServerVersionInt >= 900 then
           Result := Result + 'TOP('+IntToStr(Limit)+') ';
@@ -6711,7 +6711,8 @@ begin
           // OFFSET not supported in < 2012
           Result := Result + 'TOP ' + IntToStr(Limit) + ' ' + QueryBody;
         end;
-      end;
+      end else
+        Result := Result + QueryBody;
     end;
     ngMySQL: begin
       Result := Result + QueryBody + ' LIMIT ';