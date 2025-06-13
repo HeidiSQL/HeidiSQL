32860a5e727de0cdd67010c89d4f739b333966b9
Ansgar Becker <anse@heidisql.com>
2025-06-13 11:54:13 +0200

    Leave space for column id in grid column headers. See https://www.heidisql.com/forum.php?t=44161

--- source/main.pas
+++ source/main.pas
@@ -11151,7 +11151,10 @@ begin
   if not (coVisible in Col.Options) then
     Exit;
   ColTextWidth := Tree.Canvas.TextWidth(Col.Text);
-  // Add space for sort glyph
+  // Add space for column id ...
+  if (Column > 0) and AppSettings.ReadBool(asShowRowId) then
+    ColTextWidth := ColTextWidth + Tree.Canvas.TextWidth(Column.ToString) + 5;
+  // ... and sort glyph
   if Col.ImageIndex > -1 then
     ColTextWidth := ColTextWidth + 20;
   Node := Tree.GetFirstVisible;
