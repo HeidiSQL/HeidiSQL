bb1298c4b2a1a51a42a351269d420d05b78d511f
Ansgar Becker <anse@heidisql.com>
2025-10-06 16:48:07 +0200

    Issue #2243: suppress prompt to save a yet unknown host key in ssh mode, by disabling StrictHostKeyChecking

--- source/dbconnection.pas
+++ source/dbconnection.pas
@@ -1106,6 +1106,8 @@ begin
     SshCmd := SshCmd + IfThen(IsPlink, ' -P ', ' -p ') + IntToStr(FConnection.Parameters.SSHPort);
   if FConnection.Parameters.SSHPrivateKey <> '' then
     SshCmd := SshCmd + ' -i "' + FConnection.Parameters.SSHPrivateKey + '"';
+  if not IsPlink then
+    SshCmd := SshCmd + ' -o StrictHostKeyChecking=no';
   SshCmd := SshCmd + ' -N -L ' + IntToStr(FConnection.Parameters.SSHLocalPort) + ':' + FConnection.Parameters.Hostname + ':' + IntToStr(FConnection.Parameters.Port);
   rx := TRegExpr.Create;
   rx.Expression := '(-pw\s+")[^"]*(")';
