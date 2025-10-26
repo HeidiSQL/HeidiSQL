f2028e135e4e3ded46612d30b4687db413c2bb0f
Ansgar Becker <anse@heidisql.com>
2025-10-26 13:33:20 +0100

    fix: sticky empty-password warning after setting a non-empty one
    Refs #441

--- source/usermanager.pas
+++ source/usermanager.pas
@@ -1188,6 +1188,7 @@ var
 begin
   // Save changes
   FocusedUser := listUsers.GetNodeData(listUsers.FocusedNode);
+  FocusedUser.Problem := upNone;
   if FAdded then begin
     FocusedUser.Username := editUsername.Text;
     FocusedUser.Host := editFromHost.Text;
