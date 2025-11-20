be62fd39c80252bd626a2b25187c7be303d92263
Ansgar Becker <anse@heidisql.com>
2025-11-20 19:33:09 +0100

    Issue #2307: disable tree option asDoubleClickInsertsNodeText by default

--- source/apphelpers.pas
+++ source/apphelpers.pas
@@ -4077,7 +4077,7 @@ begin
   InitSetting(asDateTimeEditorCursorPos,          'DateTimeEditor_CursorPos_Type%s',       0);
   InitSetting(asAppLanguage,                      'Language',                              0, False, '');
   InitSetting(asAutoExpand,                       'AutoExpand',                            0, False);
-  InitSetting(asDoubleClickInsertsNodeText,       'DoubleClickInsertsNodeText',            0, True);
+  InitSetting(asDoubleClickInsertsNodeText,       'DoubleClickInsertsNodeText',            0, False);
   InitSetting(asForeignDropDown,                  'ForeignDropDown',                       0, True);
   InitSetting(asIncrementalSearch,                'IncrementalSearch',                     0, True);
   InitSetting(asQueryHistoryEnabled,              'QueryHistory',                          0, True);
