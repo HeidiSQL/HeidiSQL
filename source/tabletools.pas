HeidiSQL/HeidiSQL

commit 2e290f1be155e09ef4edb50df9ed05a772f50c5c

refactor: prefer Delphi friendly SysUtils.PathDelim over hard-coded backslash

source/tabletools.pas
@@ -1848,7 +1848,7 @@ begin
 
   if ToDir then begin
     FreeAndNil(ExportStream);
-    DbDir := IncludeTrailingPathDelimiter(GetOutputFilename(comboExportOutputTarget.Text, DBObj)) + DBObj.Database + '\';
+    DbDir := GetOutputFilename(comboExportOutputTarget.Text, DBObj) + PathDelim + DBObj.Database + PathDelim;
     if not DirectoryExists(DbDir) then
       ForceDirectories(DbDir);
     ExportStream := TFileStream.Create(DbDir + DBObj.ObjType.ToLower + '-' + DBObj.Name+'.sql', fmCreate or fmOpenWrite);

source/apphelpers.pas
@@ -3861,7 +3861,7 @@ begin
   InitSetting(asIgnoreDatabasePattern,            'IgnoreDatabasePattern',                 0, False, '', True);
   InitSetting(asLogFileDdl,                       'LogFileDdl',                            0, False, '', True);
   InitSetting(asLogFileDml,                       'LogFileDml',                            0, False, '', True);
-  InitSetting(asLogFilePath,                      'LogFilePath',                           0, False, DirnameUserAppData + 'Logs\%session\%db\%y%m%d.sql', True);
+  InitSetting(asLogFilePath,                      'LogFilePath',                           0, False, DirnameUserAppData + 'Logs'+PathDelim+'%session'+PathDelim+'%db'+PathDelim+'%y%m%d.sql', True);
   if Screen.Fonts.IndexOf('Consolas') > -1 then
     InitSetting(asFontName,                       'FontName',                              0, False, 'Consolas')
   else
@@ -3877,7 +3877,7 @@ begin
   InitSetting(asHightlightSameTextBackground,     'HightlightSameTextBackground',          GetThemeColor(clInfoBk));
   InitSetting(asLogsqlnum,                        'logsqlnum',                             300);
   InitSetting(asLogsqlwidth,                      'logsqlwidth',                           2000);
-  InitSetting(asSessionLogsDirectory,             'SessionLogsDirectory',                  0, False, DirnameUserAppData + 'Sessionlogs\');
+  InitSetting(asSessionLogsDirectory,             'SessionLogsDirectory',                  0, False, DirnameUserAppData + 'Sessionlogs' + PathDelim);
   InitSetting(asLogHorizontalScrollbar,           'LogHorizontalScrollbar',                0, False);
   InitSetting(asSQLColActiveLine,                 'SQLColActiveLine',                      0, False, 'clNone');
   InitSetting(asSQLColMatchingBraceForeground,    'SQLColMatchingBraceForeground',         0, False, 'clBlack');
@@ -4075,7 +4075,7 @@ begin
   InitSetting(asDisplayReverseForeignKeys,        'DisplayReverseForeignKeys',             0, False);
   InitSetting(asGenerateDataNumRows,              'GenerateDataNumRows',                   1000);
   InitSetting(asGenerateDataNullAmount,           'GenerateDataNullAmount',                10);
-  InitSetting(asCustomSnippetsDirectory,          'CustomSnippetsDirectory',               0, False, DirnameUserDocuments + 'Snippets\');
+  InitSetting(asCustomSnippetsDirectory,          'CustomSnippetsDirectory',               0, False, DirnameUserDocuments + 'Snippets' + PathDelim);
   InitSetting(asPromptSaveFileOnTabClose,         'PromptSaveFileOnTabClose',              0, True);
   // Restore tabs feature crashes often on old XP systems, see https://www.heidisql.com/forum.php?t=34044
   InitSetting(asRestoreTabs,                      'RestoreTabs',                           0, Win32MajorVersion >= 6);
@@ -4775,7 +4775,7 @@ end;
 function TAppSettings.DirnameBackups: String;
 begin
   // Create backup folder if it does not exist and return it
-  Result := DirnameUserAppData + 'Backups\';
+  Result := DirnameUserAppData + 'Backups' + PathDelim;
   if not DirectoryExists(Result) then begin
     ForceDirectories(Result);
   end;
@@ -4784,7 +4784,7 @@ end;
 
 function TAppSettings.DirnameHighlighters: string;
 begin
-  Result := DirnameUserAppData + 'Highlighters\';
+  Result := DirnameUserAppData + 'Highlighters' + PathDelim;
   if not DirectoryExists(Result) then begin
     ForceDirectories(Result);
   end;

source/main.pas
@@ -5302,7 +5302,7 @@ begin
   // Click on the popupQueryLoad
   Filename := (Sender as TMenuItem).Caption;
   Filename := StripHotkey(Filename);
-  if Pos('\', Filename) = 0 then // assuming we load a snippet
+  if Pos(PathDelim, Filename) = 0 then // assuming we load a snippet
     Filename := AppSettings.DirnameSnippets + Filename + FILEEXT_SNIPPET
   else begin // assuming we load a file from the recent-list
     p := Pos(' ', Filename) + 1;

commit 152ab70df6354f9749feffab976a32424abcd196

fix: invalid folder path for SQL export cannot be created if it contains a duplicated path delimiter
Custom output dir may already have a trailing path delimiter, only add when not
Broken in 2e290f1be155e09ef4edb50df9ed05a772f50c5c

Refs #2558

source/tabletools.pas
@@ -1848,7 +1848,7 @@ begin
 
   if ToDir then begin
     FreeAndNil(ExportStream);
-    DbDir := GetOutputFilename(comboExportOutputTarget.Text, DBObj) + PathDelim + DBObj.Database + PathDelim;
+    DbDir := IncludeTrailingPathDelimiter(GetOutputFilename(comboExportOutputTarget.Text, DBObj)) + DBObj.Database + PathDelim;
     if not DirectoryExists(DbDir) then
       ForceDirectories(DbDir);
     ExportStream := TFileStream.Create(DbDir + DBObj.ObjType.ToLower + '-' + DBObj.Name+'.sql', fmCreate or fmOpenWrite);