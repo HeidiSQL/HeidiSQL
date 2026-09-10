06ab070bb175c4745e9df77543280118bc6ffa71
Ansgar Becker <anse@heidisql.com>
2026-09-10 18:11:21 +0200

    feat: Use fully qualified table name in "Select Top x rows" action when a click on it is combined with Shift
    https://www.heidisql.com/forum.php?t=45359

--- source/main.pas
+++ source/main.pas
@@ -5236,6 +5236,7 @@ var
   Obj: TDBObject;
   Tab: TQueryTab;
   Conn: TDBConnection;
+  TableName: String;
 begin
   // Query table data
   Conn := ActiveConnection;
@@ -5248,7 +5249,11 @@ begin
 
   for Obj in Objects do begin
     Tab := GetOrCreateEmptyQueryTab(True);
-    Tab.Memo.Text := Conn.ApplyLimitClause('SELECT', '* FROM '+Obj.QuotedName, AppSettings.ReadInt(asDatagridRowsPerStep), 0);
+    if ssShift in GetKeyShiftState then
+      TableName := Obj.QuotedDbAndTableName
+    else
+      TableName := Obj.QuotedName;
+    Tab.Memo.Text := Conn.ApplyLimitClause('SELECT', '* FROM '+TableName, AppSettings.ReadInt(asDatagridRowsPerStep), 0);
     SetTabCaption(Tab.TabSheet.TabIndex, Obj.Name);
     actExecuteQueryExecute(Sender);
   end;
